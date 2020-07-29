c7d3dbe72597ca098ff9e20e47153fb32f2cde77
Chris Choi <xeedlab@hwbyon6110w-lp140-02-70-50-34-89.dsl.bell.ca>
2020-07-29 19:18:28 +0900

    https://www.acmicpc.net/problem/15988

--- Dynamic Programming/acmicpc.net/9095/Main.java
+++ Dynamic Programming/acmicpc.net/9095/Main.java
@@ -19,7 +19,7 @@ class Main {
         d[1] = 1;
         d[2] = 2;
         for (int i = 3; i < 11; i++) {
-            d[i] = d[i-1] + d[i-2] + d[i-3];
+            d[i] = d[i - 1] + d[i - 2] + d[i - 3];
         }
         for (int i = 0; i < T; i++) {
             int n = Integer.parseInt(br.readLine());
