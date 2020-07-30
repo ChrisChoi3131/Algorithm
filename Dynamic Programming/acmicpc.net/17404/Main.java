5a94b7d39f95a409e187102e58d7e1c284706e09
Chris Choi <xeedlab@hwbyon6110w-lp140-02-70-50-34-89.dsl.bell.ca>
2020-07-30 15:17:35 +0900

    개발중

---
+++ Dynamic Programming/acmicpc.net/17404/Main.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+class Main {
+    static final String inputFilePath = "./Dynamic Programming/acmicpc.net/17404/sample.txt";
+    // static final int mod = 1000000000;
+    // static final String inputFilePath = "./# format/sample.txt";
+
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream(inputFilePath));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int a[][] = new int[N][3];
+        int d[][] = new int[N][3];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            a[i][0] = Integer.parseInt(st.nextToken());
+            a[i][1] = Integer.parseInt(st.nextToken());
+            a[i][2] = Integer.parseInt(st.nextToken());
+        }
+    }
+}
