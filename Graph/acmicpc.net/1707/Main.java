d3502f330bade1f9dccf710fd8e16290ce316e2f
Chris Choi <xeedlab@bras-base-prthon1623w-grc-07-70-50-34-89.dsl.bell.ca>
2020-09-10 13:34:39 +0900

    1707 ing

---
+++ Graph/acmicpc.net/1707/Main.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+class Main {
+    static final String inputFilePath = "./Dynamic Programming/acmicpc.net/11726/sample.txt";
+    // static final int mod = 1000000000;
+    // static final String inputFilePath = "./# format/sample.txt";
+
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream(inputFilePath));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+    }
+}
