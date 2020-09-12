83487bba87b1eb9fbf1edfc11589253761d78dd2
chrisChoi3131 <chrischoi3131@gmail.com>
2020-09-12 11:02:55 +0900

    https://www.acmicpc.net/problem/1707

--- Graph/acmicpc.net/1707/Main.java
+++ Graph/acmicpc.net/1707/Main.java
@@ -3,20 +3,80 @@ import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 class Main {
-    static final String inputFilePath = "./Dynamic Programming/acmicpc.net/11726/sample.txt";
+    static final String inputFilePath = "./Graph/acmicpc.net/1707/sample.txt";
     // static final int mod = 1000000000;
     // static final String inputFilePath = "./# format/sample.txt";
+    static ArrayList<Integer>[] a;
+    static int[] c;
+    static final int NOTVISITED = 0;
+    static final int RED = 1;
+    static final int BLUE = 2;
 
     public static void main(String[] args) throws Exception {
         System.setIn(new FileInputStream(inputFilePath));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(br.readLine());
+        for (int test_case = 0; test_case < K; test_case++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int V = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
+            a = new ArrayList[V + 1];
+            for (int i = 1; i <= V; i++) {
+                a[i] = new ArrayList<Integer>();
+            }
+            for (int i = 1; i <= E; i++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                a[x].add(y);
+                a[y].add(x);
+            }
+            c = new int[V + 1];
+            for (int i = 1; i <= V; i++) {
+                if (c[i] == NOTVISITED) {
+                    dfs(i);
+                }
+            }
+            if (isBipartitleGraph(V)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
 
+        }
+    }
+
+    public static void dfs(int v) {
+        if (c[v] == NOTVISITED) {
+            c[v] = RED;
+        }
+        for (int i = 0; i < a[v].size(); i++) {
+            int child = a[v].get(i);
+            if (c[child] == NOTVISITED) {
+                if (c[v] == RED) {
+                    c[child] = BLUE;
+                } else if (c[v] == BLUE) {
+                    c[child] = RED;
+                }
+                dfs(child);
+            }
+        }
+    }
+
+    public static boolean isBipartitleGraph(int V) {
+        for (int i = 1; i <= V; i++) {
+            for (int j = 0; j < a[i].size(); j++) {
+                int child = a[i].get(j);
+                if (c[i] == c[child]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
