99783f2cb30848ad2ef15e589055d14f1edde6b3
ChrisChoi3131 <ChrisChoi3131@gmail.com>
2020-11-02 22:46:25 +0900

    ing~

--- # ing/Main.java
+++ # ing/Main.java
@@ -7,8 +7,11 @@ import java.util.*;
 
 class Main {
   static final String inputFilePath = "./# ing/sample.txt";
-  static boolean[] c = new boolean[10];
-  static int[] a = new int[10];
+  static boolean[] check;
+  static int[] a;
+  static int[] num;
+
+  static int n, m;
   static StringBuilder sb = new StringBuilder();
 
   public static void main(String[] args) throws Exception {
@@ -16,31 +19,39 @@ class Main {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     StringTokenizer st = new StringTokenizer(br.readLine());
-    int n = Integer.parseInt(st.nextToken());
-    int m = Integer.parseInt(st.nextToken());
-    go(0, 1, n, m);
+    n = Integer.parseInt(st.nextToken());
+    m = Integer.parseInt(st.nextToken());
+    st = new StringTokenizer(br.readLine());
+    a = new int[n];
+    num = new int[n];
+    check = new boolean[n];
+    for (int i = 0; i < n; i++) {
+      num[i] = Integer.parseInt(st.nextToken());
+    }
+    Arrays.sort(a);
+    go(0);
     System.out.println(sb.toString());
   }
 
-  static void go(int index, int start, int n, int m) {
+  static void go(int index) {
     if (index == m) {
       for (int i = 0; i < m; i++) {
         sb.append(a[i]);
-        if (i < m - 1) {
+        if (index <= m - 1) {
           sb.append(" ");
         }
       }
       sb.append("\n");
       return;
     }
-    for (int i = start; i <= n; i++) {
-      if (c[i]) {
+    for (int i = 0; i < a.length; i++) {
+      if (check[i]) {
         continue;
       }
-      c[i] = true;
-      a[index] = i;
-      go(index + 1, i + 1, n, m);
-      c[i] = false;
+      check[i] = true;
+      a[i] = num[i];
+      go(index + 1);
+      check[i] = false;
     }
   }
 }
