8ff92f6edb064e30fff43cf6f02ac7542d917662
ChrisChoi3131 <ChrisChoi3131@gmail.com>
2020-11-04 22:32:40 +0900

    sds stack ing~~

--- # ing/Solution.java
+++ # ing/Solution.java
@@ -9,42 +9,7 @@ import java.util.StringTokenizer;
 
 class Solution {
   static final String inputFilePath = "./# ing/sample.txt";
-  static ArrayList<Integer> a[];
-
-  static int n, k;
-  static boolean check[];
-  static int group[];
-  static LinkedList<int[]> s = new LinkedList<>();
-
-  static void dfs(int start) {
-    for (int i = 0; i < n; i++) {
-      check[i] = false;
-    }
-    s.clear();
-    int order = -1, i;
-
-    s.addLast(new int[] { start, order });
-    check[start] = true;
-    group[start] = start;
-
-    while (!s.isEmpty()) {
-      int curr = s.getLast()[0];
-      for (i = 0; i < a[curr].size(); i++) {
-        int child = a[curr].get(i);
-        if (!check[child]) {
-          check[child] = true;
-          group[child] = start;
-          s.addLast(new int[] { child, i });
-          order = -1;
-          break;
-        }
-      }
-      if (i == a[curr].size()) {
-        s.removeLast();
-      }
-    }
-
-  }
+  static final long NUMBER = 1000000007;
 
   public static void main(String[] args) throws Exception {
     System.setIn(new FileInputStream(inputFilePath));
@@ -52,52 +17,47 @@ class Solution {
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     int t = Integer.parseInt(br.readLine());
     for (int test_case = 1; test_case <= t; test_case++) {
+      int n = Integer.parseInt(br.readLine());
+      int a[] = new int[n];
+      int d[] = new int[n];
       StringTokenizer st = new StringTokenizer(br.readLine());
-      n = Integer.parseInt(st.nextToken());
-      k = Integer.parseInt(st.nextToken());
-      a = new ArrayList[n];
-      ArrayList<Integer> partyMember = new ArrayList<>();
-      check = new boolean[n];
-      group = new int[n];
       for (int i = 0; i < n; i++) {
-        a[i] = new ArrayList<Integer>();
-        group[i] = -1;
+        a[i] = Integer.parseInt(st.nextToken());
+        d[i] = -1;
       }
-      for (int i = 0; i < k; i++) {
-        st = new StringTokenizer(br.readLine());
-        int x = Integer.parseInt(st.nextToken()) - 1;
-        int y = Integer.parseInt(st.nextToken()) - 1;
-        a[x].add(y);
-        a[y].add(x);
-        if (!partyMember.contains(x)) {
-          partyMember.add(x);
+      LinkedList<int[]> s = new LinkedList<int[]>();
+      s.addLast(new int[] { a[n - 1], n - 1 });
+      int currIdx = n - 1;
+      while (true) {
+        if (currIdx == 0) {
+          break;
         }
-        if (!partyMember.contains(y)) {
-          partyMember.add(y);
+        currIdx--;
+        int top = s.getLast()[0];
+        int topIdx = s.getLast()[1];
+        if (a[currIdx] < top) {
+          s.addLast(new int[] { a[currIdx], currIdx });
+        } else {
+          d[topIdx] = currIdx;
+          s.removeLast();
+          while (!s.isEmpty()) {
+            top = s.getLast()[0];
+            topIdx = s.getLast()[1];
+            if (a[currIdx] < top) {
+              break;
+            } else {
+              d[topIdx] = currIdx;
+              s.removeLast();
+            }
+          }
+          s.addLast(new int[] { a[currIdx], currIdx });
         }
       }
-
-      int cntGroups = 0;
-      int maxPartyMember = 0;
+      long ans = 0;
       for (int i = 0; i < n; i++) {
-        if (group[i] == -1 && partyMember.contains(i)) {
-          dfs(i);
-          cntGroups++;
-        }
+        ans = (ans + d[i] + 1) % NUMBER;
       }
-      int numPartymember[] = new int[n];
-      for (int i = 0; i < n; i++) {
-        if (group[i] != -1) {
-          numPartymember[group[i]]++;
-        }
-      }
-      for (int i = 0; i < n; i++) {
-        if (numPartymember[i] > maxPartyMember) {
-          maxPartyMember = numPartymember[i];
-        }
-      }
-
-      System.out.println("#" + test_case + " " + cntGroups + " " + maxPartyMember);
+      System.out.println(ans);
     }
   }
 }
