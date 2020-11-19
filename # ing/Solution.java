c040df8007a333fb70e11ea2a631f542c0a5643e
Chris Choi <chrischoi@Chriss-iMac.local>
2020-11-20 08:42:48 +0900

    [연습A-0028] 나누기 푸는중

--- # ing/Solution.java
+++ # ing/Solution.java
@@ -3,14 +3,13 @@ import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 class Solution {
   static final String inputFilePath = "./# ing/sample.txt";
+  static int ans0 = 0;
+  static int ans1 = 0;
+  static int a[][];
 
   public static void main(String[] args) throws Exception {
     System.setIn(new FileInputStream(inputFilePath));
@@ -18,39 +17,50 @@ class Solution {
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     int t = Integer.parseInt(br.readLine());
     for (int test_case = 1; test_case <= t; test_case++) {
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      int n = Integer.parseInt(st.nextToken());
-      int m = Integer.parseInt(st.nextToken());
-      ArrayList<Integer> a = new ArrayList<Integer>();
-      ArrayList<Integer> b = new ArrayList<Integer>();
-      st = new StringTokenizer(br.readLine());
-      for (int i = 0; i < n; i++) {
-        a.add(Integer.parseInt(st.nextToken()));
-      }
-      st = new StringTokenizer(br.readLine());
-      for (int i = 0; i < m; i++) {
-        b.add(Integer.parseInt(st.nextToken()));
-      }
-      int ans = 0;
-      while (!a.isEmpty() && !b.isEmpty()) {
-        int min = Integer.MAX_VALUE;
-        int aIdx = -1;
-        int bIdx = -1;
-        for (int i = 0; i < a.size(); i++) {
-          for (int j = 0; j < b.size(); j++) {
-            int value = Math.abs(a.get(i) - b.get(j));
-            if (min > value) {
-              min = value;
-              aIdx = i;
-              bIdx = j;
-            }
-          }
+      ans0 = 0;
+      ans1 = 0;
+      int n = Integer.parseInt(br.readLine());
+      a = new int[n + 1][n + 1];
+      for (int i = 1; i <= n; i++) {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int j = 1; j <= n; j++) {
+          a[i][j] = Integer.parseInt(st.nextToken());
         }
-        ans += min;
-        a.remove(aIdx);
-        b.remove(bIdx);
       }
-      System.out.println("#" + test_case + " " + ans);
+      recur(1, n, 1, n);
+      System.out.println("#" + test_case + " " + ans0 + " " + ans1);
+    }
+  }
+
+  static void recur(int hStart, int hEnd, int wStart, int wEnd) {
+    System.out.println(hStart + " " + hEnd + " " + wStart + " " + wEnd);
+    if (hStart >= hEnd || wStart >= wEnd) {
+      if (ans0 == a[hStart][wStart]) {
+        ans0++;
+      } else {
+        ans1++;
+      }
+      return;
+    }
+    boolean check = false;
+    for (int i = hStart; i <= hEnd; i++) {
+      for (int j = wStart; j <= wEnd; j++) {
+        if (a[hStart][wStart] != a[i][j]) {
+          recur(hStart, hEnd / 2, wStart, wEnd / 2);
+          recur(hStart, hEnd / 2, wEnd / 2 + 1, wEnd);
+          recur(hEnd / 2 + 1, hEnd, wStart, wEnd / 2);
+          recur(hEnd / 2 + 1, hEnd, wEnd / 2 + 1, wEnd);
+          check = true;
+          break;
+        }
+      }
+    }
+    if (!check) {
+      if (ans0 == a[hStart][hEnd]) {
+        ans0++;
+      } else {
+        ans1++;
+      }
     }
   }
 }
