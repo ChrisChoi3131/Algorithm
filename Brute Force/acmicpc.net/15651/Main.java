bc4c0c0af6f16aa83b6108b6dd4bb050d115de9b
Chris Choi <chrischoi@Chriss-MacBook-Pro.local>
2020-11-25 13:20:55 +0900

    https://www.acmicpc.net/problem/15651

--- Brute Force/acmicpc.net/15651/Main.java
+++ Brute Force/acmicpc.net/15651/Main.java
@@ -1,41 +1,45 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.*;
 
 class Main {
   static final String inputFilePath = "./# ing/sample.txt";
-  static boolean[] c = new boolean[10];
-  static int[] a = new int[10];
-  static StringBuilder sb = new StringBuilder();
+  static StringTokenizer st;
+  static StringBuilder sb;
+  static int n, m;
+  static boolean c[];
+  static int a[];
 
   public static void main(String[] args) throws Exception {
     System.setIn(new FileInputStream(inputFilePath));
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    int n = Integer.parseInt(st.nextToken());
-    int m = Integer.parseInt(st.nextToken());
-    go(0, 1, n, m);
+    st = new StringTokenizer(br.readLine());
+    n = Integer.parseInt(st.nextToken());
+    m = Integer.parseInt(st.nextToken());
+    c = new boolean[n + 1];
+    a = new int[m];
+    sb = new StringBuilder();
+    recur(0);
     System.out.println(sb.toString());
   }
 
-  static void go(int index, int start, int n, int m) {
+  static void recur(int index) {
     if (index == m) {
       for (int i = 0; i < m; i++) {
+        // System.out.print(a[i]);
         sb.append(a[i]);
-        if (i < m - 1) {
+        if (i != m - 1) {
+          // System.out.print(" ");
           sb.append(" ");
         }
       }
+      // System.out.println();
       sb.append("\n");
       return;
     }
     for (int i = 1; i <= n; i++) {
       a[index] = i;
-      go(index + 1, i + 1, n, m);
+      recur(index + 1);
     }
   }
 }
