9d3fd33771458678f7d75f583247e66c6f1e79c6
Chris Choi <chrischoi3131@gmail.com>
2021-03-21 22:11:04 +0900

    https://www.acmicpc.net/problem/11724 JS

---
+++ Graph/acmicpc.net/11724/Main.js
@@ -0,0 +1,31 @@
+const inputFilePath = "/sample.txt";
+let input = require("fs").readFileSync(__dirname + inputFilePath).toString().trim().split("\n");
+// let input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
+
+const [N, M] = input[0].split(" ").map(Number);
+let list = Array.from(Array(N + 1), () => []);
+for (let i = 1; i <= M; i++) {
+  const [x, y] = input[i].split(" ").map(Number);
+  list[x].push(y);
+  list[y].push(x);
+}
+let c = Array.from(Array(N + 1), () => false);
+let groups = Array.from(Array(N + 1), () => 0);
+for (let i = 1; i <= N; i++) {
+  dfs(i, i);
+}
+let counts = {};
+for (let i = 1; i < groups.length; i++) {
+  let num = groups[i];
+  counts[num] = counts[num] ? counts[num] + 1 : 1;
+}
+console.log(Object.keys(counts).length);
+
+function dfs(node, groupNum) {
+  if (c[node]) return;
+  groups[node] = groupNum;
+  c[node] = true;
+  for (const next of list[node]) {
+    if (!c[next]) dfs(next, groupNum);
+  }
+}
