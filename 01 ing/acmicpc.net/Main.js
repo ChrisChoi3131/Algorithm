d73506049ea7c5f8be9ce04aaff324b554a38d03
Chris Choi <chrischoi3131@gmail.com>
2021-04-03 18:50:20 +0900

    set env.

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -1,41 +1,59 @@
+
 const inputFilePath = "/sample.txt";
-let input = require("fs").readFileSync(__dirname + inputFilePath);
-// let input = require("fs").readFileSync('/dev/stdin');
-input = input.toString().trim().split("\n");
-let firstLine = input[0].split(" ");
-const N = Number(firstLine[0]);
-const M = Number(firstLine[1]);
-let array = input[1].split(" ").map(x => Number(x)).sort((a, b) => a - b);
-array = cal(array);
-let c = array[1];
-array = array[0];
-let a = [];
-let ans = [];
-go(0, 0);
-console.log(ans.join("\n"));
+let input = require("fs").readFileSync(__dirname + inputFilePath).toString().trim().split("\n");
+// let input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
+const [M, N] = input[0].split(" ").map(Number);
+const DX = [-1, 0, 0, 1];
+const DY = [0, -1, 1, 0];
+const q = new Array(1005).fill(0);
+let head = 0;
+let tail = 0;
 
-function cal(array) {
-  let a = [], b = [], prev;
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] !== prev) {
-      a.push(array[i]);
-      b.push(1);
-    } else {
-      b[b.length - 1]++;
+const q_push = (dat) => {
+  q[tail++] = dat;
+};
+
+const q_shift = () => {
+  return q[head++];
+};
+const q_size = () => tail - head;
+
+let list = [];
+let c = [];
+for (let i = 1; i <= N; i++) {
+  let line = input[i].split(" ").map(Number);
+  list.push(line);
+  c.push(line);
+}
+
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < M; j++) {
+    if (list[i][j] === 1) {
+      q_push({ x: i, y: j });
     }
-    prev = array[i];
   }
-  return [a, b];
+}
+while (q_size() > 0) {
+  let curr = q_shift();
+  for (let i = 0; i < 4; i++) {
+    const nextX = curr.x + DX[i];
+    const nextY = curr.y + DY[i];
+    if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= M) continue;
+    if (c[nextX][nextY] === 0) {
+      c[nextX][nextY] = c[curr.x][curr.y] + 1;
+      q_push({ x: nextX, y: nextY });
+    }
+  }
 }
 
-function go(index, start) {
-  if (index === M) {
-    ans.push(a.join(" "));
-    return;
-  }
-  for (let i = 0; i < array.length; i++) {
-    if (index > 0 && a[index - 1] > array[i]) continue;
-    a[index] = array[i];
-    go(index + 1);
+let max = 0;
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < M; j++) {
+    if (list[i][j] === 0) {
+      console.log(-1);
+      return;
+    }
+    if (list[i][j] > max) max = list[i][j];
   }
 }
+console.log(max - 1);
