06b77de20f816e62001bdaf97d1d4dfa3f511901
Chris Choi <chrischoi3131@gmail.com>
2021-06-10 21:18:48 +0900

    eslint init

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  extends: ['plugin:prettier/recommended'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      { singleQuote: true, useTabs: false, tabWidth: 2 },
+    ],
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off',
+  },
+};
