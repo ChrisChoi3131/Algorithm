580d24d99c20ddb0122be872f7afc4cb39d79cee
Chris Choi <chrischoi3131@gmail.com>
2021-06-11 15:54:22 +0900

    https://www.acmicpc.net/problem/9012 JS

--- 01 ing/acmicpc.net/Data Structure/Stack.js
+++ 01 ing/acmicpc.net/Data Structure/Stack.js
@@ -2,31 +2,31 @@ class Stack {
   constructor() {
     this._arr = [];
   }
-  push(value) {
+  ["push"](value) {
     return this._arr.push(value);
   }
-  pop() {
+  ["pop"]() {
     if (this.isEmpty()) {
       return -1;
     } else {
       return this._arr.pop();
     }
   }
-  size() {
-    return this._arr.length();
+  ["size"]() {
+    return this._arr.length;
   }
-  isEmpty() {
-    if (this._arr.length() === 0) {
+  ["isEmpty"]() {
+    if (this._arr.length === 0) {
       return 1;
     } else {
       return 0;
     }
   }
-  top() {
+  ["top"]() {
     if (this.isEmpty()) {
       return -1;
     } else {
-      return this._arr[this._arr.length() - 1];
+      return this._arr[this._arr.length - 1];
     }
   }
 }
