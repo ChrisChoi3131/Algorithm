1cd93ee5c62c8c6c266832b1d693e42af8a8fbbc
Chris Choi <chrischoi3131@gmail.com>
2021-08-31 22:31:25 +0900

    https://www.acmicpc.net/problem/10828 Python3

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -1,37 +1,40 @@
+"use strict";
 const inputFilePath = "/sample.txt";
-let input = require("fs").readFileSync(__dirname + inputFilePath).toString().trim().split("\n");
-// let input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
-let lineIdx = 0;
-const T = Number(input[lineIdx++]);
-for (let test_case = 0; test_case < T; test_case++) {
-  const [V, E] = input[lineIdx++].split(" ").map(Number);
-  let list = Array.from(Array(V + 1), () => []);
-  let check = Array.from(Array(V + 1)).fill(0);
-  for (let i = 0; i < E; i++) {
-    const [X, Y] = input[lineIdx++].split(" ").map(Number);
-    list[X].push(Y);
-    list[Y].push(X);
-  }
-  let ans = "YES";
-  dfs(1, 1);
-  console.log(ans);
-  function dfs(curr, color) {
-    if (check[curr] !== 0) return;
-    check[curr] = color;
-    for (let i = 0; i < list[curr].length; i++) {
-      const next = list[curr][i];
-      if (check[next] === 0) {
-        if (color === 1) {
-          dfs(next, 2);
-        } else {
-          dfs(next, 1);
-        }
-      } else if (check[next] === color) {
-        ans = "NO"
-        return;
-      }
+// const input = require("fs")
+//   .readFileSync(__dirname + inputFilePath)
+//   .toString()
+//   .trim()
+//   .split("\n");
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const N = input[0];
+const stack = [];
+const ans = [];
+
+for (let i = 1; i < input.length; i++) {
+  const element = input[i].split(" ");
+  if (element[0] === "push") {
+    stack.push(element[1]);
+  } else if (element[0] === "pop") {
+    if (stack.length === 0) {
+      ans.push(-1);
+    } else {
+      ans.push(stack.pop());
+    }
+  } else if (element[0] === "size") {
+    ans.push(stack.length);
+  } else if (element[0] === "empty") {
+    if (stack.length === 0) {
+      ans.push(1);
+    } else {
+      ans.push(0);
+    }
+  } else if (element[0] === "top") {
+    if (stack.length === 0) {
+      ans.push(-1);
+    } else {
+      ans.push(stack[stack.length - 1]);
     }
   }
 }
-
-
+console.log(ans.join("\n"));
