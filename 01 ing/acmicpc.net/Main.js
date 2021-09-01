862814c09a20c58023c65339aa24e25542da82f8
Chris Choi <chrischoi3131@gmail.com>
2021-09-01 10:57:06 +0900

    https://www.acmicpc.net/problem/9093 Python3

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -1,40 +1,22 @@
 "use strict";
+
 const inputFilePath = "/sample.txt";
-// const input = require("fs")
-//   .readFileSync(__dirname + inputFilePath)
-//   .toString()
-//   .trim()
-//   .split("\n");
-const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = require("fs")
+  .readFileSync(__dirname + inputFilePath)
+  .toString()
+  .trim()
+  .split("\n");
+// const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
 
 const N = input[0];
-const stack = [];
 const ans = [];
-
-for (let i = 1; i < input.length; i++) {
-  const element = input[i].split(" ");
-  if (element[0] === "push") {
-    stack.push(element[1]);
-  } else if (element[0] === "pop") {
-    if (stack.length === 0) {
-      ans.push(-1);
-    } else {
-      ans.push(stack.pop());
-    }
-  } else if (element[0] === "size") {
-    ans.push(stack.length);
-  } else if (element[0] === "empty") {
-    if (stack.length === 0) {
-      ans.push(1);
-    } else {
-      ans.push(0);
-    }
-  } else if (element[0] === "top") {
-    if (stack.length === 0) {
-      ans.push(-1);
-    } else {
-      ans.push(stack[stack.length - 1]);
-    }
-  }
+for (let i = 1; i <= N; i++) {
+  const s = input[i].split(" ");
+  const reverseS = [];
+  s.forEach((element) => {
+    reverseS.push(element.split("").reverse().join(""));
+  });
+  ans.push(reverseS.join(" "));
 }
+
 console.log(ans.join("\n"));
