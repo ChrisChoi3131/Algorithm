a6e4553408e3af7d97a18eea74aa7ed42a040b6a
Chris Choi <chrischoi3131@gmail.com>
2021-09-06 08:25:49 +0900

    https://www.acmicpc.net/problem/10799 JS, python3

--- 01 ing/LeetCode/Main.js
+++ 01 ing/LeetCode/Main.js
@@ -1,40 +1,11 @@
 "use strict";
-const inputFilePath = "/sample.txt";
-const input = require("fs")
-  .readFileSync(__dirname + inputFilePath)
-  .toString()
-  .trim()
-  .split("\n");
+// const inputFilePath = "/sample.txt";
+// const input = require("fs")
+//   .readFileSync(__dirname + inputFilePath)
+//   .toString()
+//   .trim()
+//   .split("\n");
 // const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
 
-const N = input[0];
-const stack = [];
-const ans = [];
-
-for (let i = 1; i < N; i++) {
-  const element = input[i].split(" ");
-  if (element[0] === "push") {
-    stack.push(element[1]);
-  } else if (element[0] === "pop") {
-    if (stack.length === 0) {
-      ans.push(-1);
-    } else {
-      ans.push(stack.pop());
-    }
-  } else if (element[0] === "size") {
-    ans.push(stack.length);
-  } else if (element[0] === "empty") {
-    if (stack.length === 0) {
-      ans.push(1);
-    } else {
-      ans.push(0);
-    }
-  } else if (element[0] === "top") {
-    if (stack.length === 0) {
-      ans.push(-1);
-    } else {
-      ans.push(stack[stack.length - 1]);
-    }
-  }
-}
-console.log(ans.join("\n"));
+const nums = [2, 7, 11, 15];
+const target = 9;
