fbf99d4462bf06c69d08a95acd06a7da4a5bacd8
Chris Choi <chrischoi3131@gmail.com>
2021-09-16 09:26:17 +0900

    https://www.acmicpc.net/problem/1676 JS

--- 01 ing/LeetCode/Main.js
+++ 01 ing/LeetCode/Main.js
@@ -9,3 +9,19 @@
 
 const nums = [2, 7, 11, 15];
 const target = 9;
+
+var twoSum = function (nums, target) {
+  const ans = [];
+  outer: for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (i !== j && nums[i] + nums[j] === target) {
+        ans.push(i);
+        ans.push(j);
+        break outer;
+      }
+    }
+  }
+  return ans;
+};
+
+twoSum(nums, target);
