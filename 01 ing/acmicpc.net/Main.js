7f2cd97ae57598078c215d3b4f36cc8837373563
Chris Choi <chrischoi3131@gmail.com>
2021-10-22 10:34:33 +0900

    https://www.acmicpc.net/problem/1212 python3

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -1,19 +1,23 @@
 const inputFilePath = "/sample.txt";
-let input = require("fs").readFileSync(__dirname + inputFilePath).toString().trim().split("\n");
+let input = require("fs")
+  .readFileSync(__dirname + inputFilePath)
+  .toString()
+  .trim()
+  .split("\n");
 // let input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
-const setForFirst = ["0","1","10","11","100","101","110","111"];
-const set = ["000","001","010","011","100","101","110","111"];
+const setForFirst = ["0", "1", "10", "11", "100", "101", "110", "111"];
+const set = ["000", "001", "010", "011", "100", "101", "110", "111"];
 
 const nums = input[0].split("");
 
 let isFirst = true;
 let ans = "";
-nums.forEach(num => {
-  if(isFirst){
-    ans+=setForFirst[num];
+nums.forEach((num) => {
+  if (isFirst) {
+    ans += setForFirst[num];
     isFirst = !isFirst;
-  }else{
-    ans+=set[num];
+  } else {
+    ans += set[num];
   }
 });
-console.log(ans)
+console.log(ans);
