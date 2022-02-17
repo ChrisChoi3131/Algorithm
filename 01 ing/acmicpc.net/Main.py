16c3bb6db20edd6e7ae40ce2934ee8e5d74114df
Chris Choi <chrischoi3131@gmail.com>
2022-02-17 13:38:12 -0800

    https://www.acmicpc.net/problem/11653 Python3

--- 01 ing/acmicpc.net/Main.py
+++ 01 ing/acmicpc.net/Main.py
@@ -1,19 +1,17 @@
 # import sys
 # input = sys.stdin.readline
+
 file = open("./01 ing/acmicpc.net/sample.txt", "r")
 input = file.readline
 # N, B = map(int, input().split())
 
-#!/usr/bin/python3
-def convert(num, base):
-    if num == 0:
-        return
-    convert(num//base, base)
-    print(num%base, end=' ')
-a, b = map(int, input().split())
 n = int(input())
-ans = 0
-num = list(map(int,input().split()))
-for x in num:
-    ans = ans * a + x
-convert(ans, b)
+i = 2
+while i*i <= n:
+    while n % i == 0:
+        print(i)
+        n //= i
+    i+=1                    
+
+if(n > 1):
+    print(int(n))
