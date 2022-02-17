96632515c1d76c8dbf2566eabd5629c38b38e418
Chris Choi <chrischoi3131@gmail.com>
2022-02-17 08:20:25 -0800

    https://www.acmicpc.net/problem/11653 JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -6,27 +6,14 @@ let input = require("fs")
   .trim()
   .split("\n");
 // let input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
-const [A, B] = input[0].split(" ").map(Number);
-let m = Number(input[1]);
-const numbers = input[2].split(" ").map(Number);
 
-function convertBase(num, base) {
-  let converted = "";
-  let remainder = 0;
-  let quotient = 0;
-
-  while (num > 0) {
-    quotient = Math.floor(num / base);
-    remainder = num % base;
-    converted = remainder + " " + converted;
-    num = quotient;
+let n = Number(input[0]);
+let res = [];
+for (let i = 2; i * i <= n; i++) {
+  while (n % i === 0) {
+    res.push(i);
+    n /= i;
   }
-  return converted;
 }
-let demicalNum = 0;
-numbers.forEach((number) => {
-  demicalNum += number * Math.pow(A, --m);
-});
-
-const result = convertBase(demicalNum, B);
-console.log(result);
+if (n > 1) res.push(n);
+console.log(res.join("\n"));
