042c59272c6d8704efbf55fcb7a3c9d5161e4f81
Chris Choi <chrischoi3131@gmail.com>
2022-02-22 12:29:16 -0800

    https://www.acmicpc.net/problem/9095 JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -7,10 +7,20 @@ let input = require("fs")
   .split("\n");
 // let input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
 
-const N = Number(input[0]);
-let d = [];
-d[0] = d[1] = 1;
-for (let i = 2; i <= N; i++) {
-  d[i] = (d[i - 1] + d[i - 2] * 2) % 10007;
+const T = Number(input[0]);
+
+let d = new Array(11);
+d[0] = [0, 0, 0, 0];
+for (let i = 1; i < 11; i++) {
+  d[i] = [0, 0, 0, 0];
+  for (let j = 1; j <= 3; j++) {
+    if (i < j) continue;
+    d[i][j] = d[i - j][1] + d[i - j][2] + d[i - j][3];
+    if (i === j) d[i][j] = 1;
+  }
 }
-console.log(d[N]);
+
+for (let test_case = 1; test_case <= T; test_case++) {
+  let num = Number(input[test_case]);
+  console.log(d[num][1] + d[num][2] + d[num][3]);
+}
