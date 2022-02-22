ec0d3801775ed3330cf84de714b0d3168a8a8bfa
Chris Choi <chrischoi3131@gmail.com>
2022-02-22 11:30:53 -0800

    https://www.acmicpc.net/problem/11726 JS, Python3

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -8,13 +8,9 @@ let input = require("fs")
 // let input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
 
 const N = Number(input[0]);
-let d = new Array(N + 1);
-d[1] = 0;
-
+let d = [];
+d[0] = d[1] = 1;
 for (let i = 2; i <= N; i++) {
-  d[i] = d[i - 1];
-  if (i % 2 == 0 && d[i] > d[i / 2]) d[i] = d[i / 2];
-  if (i % 3 == 0 && d[i] > d[i / 3]) d[i] = d[i / 3];
-  d[i]++;
+  d[i] = (d[i - 1] + d[i - 2]) % 10007;
 }
 console.log(d[N]);
