623e79c58e7e0d71d3b19afca1abf07afd7e6b6c
Chris Choi <chrischoi3131@gmail.com>
2022-02-23 00:22:31 -0800

    https://www.acmicpc.net/problem/11052 DP, Python3

--- 01 ing/acmicpc.net/Main.py
+++ 01 ing/acmicpc.net/Main.py
@@ -1,20 +1,19 @@
 # import sys
 # input = sys.stdin.readline
 
+from collections import deque
+
+
 file = open("./01 ing/acmicpc.net/sample.txt", "r")
 input = file.readline
-T = int(input())
 
-d = [0] * 11
-d[0] = 1
-for i in range(1, 11):
-    if i-1 >= 0:
-        d[i] += d[i-1]
-    if i-2 >= 0:
-        d[i] += d[i-2]
-    if i-3 >= 0:
-        d[i] += d[i-3]
+N = int(input())
+p = deque(map(int, input().split(" ")))
+p.appendleft(0)
+d = [0] * (N+1)
 
-for i in range(T):
-    num = int(input())
-    print(d[num])
+for i in range(1, N+1):
+    for j in range(1,i+1):
+        if(d[i] < d[i-j] + p[j]):
+            d[i] = d[i-j] + p[j]
+print(d[N])
