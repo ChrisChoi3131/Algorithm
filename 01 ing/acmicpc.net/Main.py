e2d9009807b18dd12067bb285c6ee6b062999bf4
Chris Choi <chrischoi3131@gmail.com>
2022-02-23 00:38:30 -0800

    https://www.acmicpc.net/problem/16194 DP, Python3

--- 01 ing/acmicpc.net/Main.py
+++ 01 ing/acmicpc.net/Main.py
@@ -3,17 +3,16 @@
 
 from collections import deque
 
-
 file = open("./01 ing/acmicpc.net/sample.txt", "r")
 input = file.readline
 
 N = int(input())
 p = deque(map(int, input().split(" ")))
 p.appendleft(0)
-d = [0] * (N+1)
+d = p
 
 for i in range(1, N+1):
     for j in range(1,i+1):
-        if(d[i] < d[i-j] + p[j]):
+        if(d[i] > d[i-j] + p[j]):
             d[i] = d[i-j] + p[j]
 print(d[N])
