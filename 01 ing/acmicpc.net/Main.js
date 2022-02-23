25557f6d8d1a64212b2b064adf90be0f8af20bfd
Chris Choi <chrischoi3131@gmail.com>
2022-02-23 00:07:14 -0800

    https://www.acmicpc.net/problem/11052 DP, JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -6,21 +6,15 @@ let input = require("fs")
   .trim()
   .split("\n");
 // let input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
-
-const T = Number(input[0]);
-
-let d = new Array(11);
-d[0] = [0, 0, 0, 0];
-for (let i = 1; i < 11; i++) {
-  d[i] = [0, 0, 0, 0];
-  for (let j = 1; j <= 3; j++) {
-    if (i < j) continue;
-    d[i][j] = d[i - j][1] + d[i - j][2] + d[i - j][3];
-    if (i === j) d[i][j] = 1;
+const N = Number(input[0]);
+let P = input[1].split(" ").map(Number);
+P.unshift(0);
+let d = new Array(N + 1).fill(0);
+for (let i = 1; i <= N; i++) {
+  for (let j = 1; j <= i; j++) {
+    if (d[i] < d[i - j] + P[j]) {
+      d[i] = d[i - j] + P[j];
+    }
   }
 }
-
-for (let test_case = 1; test_case <= T; test_case++) {
-  let num = Number(input[test_case]);
-  console.log(d[num][1] + d[num][2] + d[num][3]);
-}
+console.log(d[N]);
