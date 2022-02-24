81fd805b70a7b56b7bfbc97164bb4fbf6a915082
Chris Choi <chrischoi3131@gmail.com>
2022-02-23 16:56:29 -0800

    https://www.acmicpc.net/problem/15990 DP, Python3

--- 01 ing/acmicpc.net/Main.py
+++ 01 ing/acmicpc.net/Main.py
@@ -6,13 +6,15 @@ from collections import deque
 file = open("./01 ing/acmicpc.net/sample.txt", "r")
 input = file.readline
 
-N = int(input())
-p = deque(map(int, input().split(" ")))
-p.appendleft(0)
-d = p
+T = int(input())
+num = 1000000009
+d = [[0]*4 for _ in range(100001)]
+d[1][1], d[2][2], d[3][1], d[3][2], d[3][3] = 1, 1, 1, 1, 1
+for i in range(4, len(d)):
+    d[i][1] = (d[i-1][2] + d[i-1][3]) % num
+    d[i][2] = (d[i-2][1] + d[i-2][3]) % num
+    d[i][3] = (d[i-3][1] + d[i-3][2]) % num
 
-for i in range(1, N+1):
-    for j in range(1,i+1):
-        if(d[i] > d[i-j] + p[j]):
-            d[i] = d[i-j] + p[j]
-print(d[N])
+for test_case in range(T):
+    N = int(input())
+    print((d[N][1] + d[N][2] + d[N][3]) % num)
