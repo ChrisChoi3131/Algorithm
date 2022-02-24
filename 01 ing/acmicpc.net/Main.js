30d49bc2ff84869d161b0d31cfec7066e61c5bbf
Chris Choi <chrischoi3131@gmail.com>
2022-02-23 16:21:57 -0800

    https://www.acmicpc.net/problem/15990 DP, JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -6,15 +6,17 @@ let input = require("fs")
   .trim()
   .split("\n");
 // let input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
-const N = Number(input[0]);
-let P = input[1].split(" ").map(Number);
-P.unshift(0);
-let d = [...P];
-for (let i = 1; i <= N; i++) {
-  for (let j = 1; j <= i; j++) {
-    if (d[i] > d[i - j] + P[j]) {
-      d[i] = d[i - j] + P[j];
-    }
-  }
+const T = Number(input[0]);
+const num = 1000000009;
+let ans = [];
+let d = [];
+d.push([0, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 1, 1, 1]);
+for (let i = 4; i <= 100000; i++) {
+  d.push([0, (d[i - 1][2] + d[i - 1][3]) % num, (d[i - 2][1] + d[i - 2][3]) % num, (d[i - 3][1] + d[i - 3][2]) % num]);
 }
-console.log(d[N]);
+
+for (let test_case = 1; test_case <= T; test_case++) {
+  let N = Number(input[test_case]);
+  ans.push((d[N][1] + d[N][2] + d[N][3]) % num);
+}
+console.log(ans.join("\n"));
