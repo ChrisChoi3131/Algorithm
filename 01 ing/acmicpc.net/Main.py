ac893ee5c5da37dee7eaadcaedc9cba002533c91
Chris Choi <chrischoi3131@gmail.com>
2022-02-24 05:02:58 -0800

    https://www.acmicpc.net/problem/10844 DP, Python3

--- 01 ing/acmicpc.net/Main.py
+++ 01 ing/acmicpc.net/Main.py
@@ -1,20 +1,21 @@
 # import sys
 # input = sys.stdin.readline
-
-from collections import deque
-
 file = open("./01 ing/acmicpc.net/sample.txt", "r")
 input = file.readline
 
-T = int(input())
-num = 1000000009
-d = [[0]*4 for _ in range(100001)]
-d[1][1], d[2][2], d[3][1], d[3][2], d[3][3] = 1, 1, 1, 1, 1
-for i in range(4, len(d)):
-    d[i][1] = (d[i-1][2] + d[i-1][3]) % num
-    d[i][2] = (d[i-2][1] + d[i-2][3]) % num
-    d[i][3] = (d[i-3][1] + d[i-3][2]) % num
+N = int(input())
+d = [[0] * 10 for _ in range(N+1)]
+d[1] = [1 for _ in range(10)]
+d[1][0] = 0
+MOD = 1000000000
 
-for test_case in range(T):
-    N = int(input())
-    print((d[N][1] + d[N][2] + d[N][3]) % num)
+for i in range(2, N+1):
+    for j in range(0, 10):
+        if(j == 0):
+            d[i][j] = d[i-1][1]
+        elif(j==9):
+            d[i][j] = d[i-1][8]
+        else:
+            d[i][j] = d[i-1][j-1] + d[i-1][j+1]
+        d[i][j] %= MOD
+print(sum(d[N])%MOD)
