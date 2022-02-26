ca7d892856be122b8df7d3e902d8c8324cb824ba
Chris Choi <chrischoi3131@gmail.com>
2022-02-26 11:33:51 -0800

    https://www.acmicpc.net/problem/2193 DP, Python3, JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -7,20 +7,12 @@ let input = require("fs")
   .split("\n");
 // let input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
 const N = Number(input[0]);
-const MOD = 1000000000;
 let d = [[]];
-d.push([0, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
-
+d.push([0, 1]);
 for (let i = 2; i <= N; i++) {
   d.push([]);
-  for (let j = 0; j < 10; j++) {
-    if (j === 0) {
-      d[i][j] = d[i - 1][1] % MOD;
-    } else if (j === 9) {
-      d[i][j] = d[i - 1][8] % MOD;
-    } else {
-      d[i][j] = (d[i - 1][j - 1] + d[i - 1][j + 1]) % MOD;
-    }
-  }
+  d[i][0] = BigInt(d[i - 1][0] + d[i - 1][1]);
+  d[i][1] = BigInt(d[i - 1][0]);
 }
-console.log(d[N].reduce((p, c) => p + c) % MOD);
+
+console.log((d[N][0] + d[N][1]).toString());
--- 01 ing/acmicpc.net/Main.py
+++ 01 ing/acmicpc.net/Main.py
@@ -3,19 +3,10 @@
 file = open("./01 ing/acmicpc.net/sample.txt", "r")
 input = file.readline
 
-N = int(input())
-d = [[0] * 10 for _ in range(N+1)]
-d[1] = [1 for _ in range(10)]
-d[1][0] = 0
-MOD = 1000000000
-
-for i in range(2, N+1):
-    for j in range(0, 10):
-        if(j == 0):
-            d[i][j] = d[i-1][1]
-        elif(j==9):
-            d[i][j] = d[i-1][8]
-        else:
-            d[i][j] = d[i-1][j-1] + d[i-1][j+1]
-        d[i][j] %= MOD
-print(sum(d[N])%MOD)
+d = [0]*91
+n = int(input())
+d[1] = 1
+d[2] = 1
+for i in range(3, n+1):
+    d[i] = d[i-1] + d[i-2]
+print(d[n])
