d1c78e4c315db5b96e78aece031a6ac8179ebccf
Chris Choi <chrischoi3131@gmail.com>
2022-02-27 01:37:21 -0800

    https://www.acmicpc.net/problem/11053 DP, Python3

--- 01 ing/acmicpc.net/Main.py
+++ 01 ing/acmicpc.net/Main.py
@@ -3,10 +3,13 @@
 file = open("./01 ing/acmicpc.net/sample.txt", "r")
 input = file.readline
 
-d = [0]*91
-n = int(input())
-d[1] = 1
-d[2] = 1
-for i in range(3, n+1):
-    d[i] = d[i-1] + d[i-2]
-print(d[n])
+N = int(input())
+A = list(map(int, input().split(" ")))
+d = [1] * N
+
+for i in range(0, N):
+    for j in range(0, i):
+        if(A[i] > A[j] and d[i] <= d[j]):
+            d[i] = d[j] + 1;
+
+print(max(d))
