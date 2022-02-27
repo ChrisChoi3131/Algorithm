ef8e5df4349f8e47f9fc0649f19ad6d066b976aa
Chris Choi <chrischoi3131@gmail.com>
2022-02-27 01:20:01 -0800

    https://www.acmicpc.net/problem/11053 DP, JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -6,13 +6,16 @@ let input = require("fs")
   .trim()
   .split("\n");
 // let input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
+
 const N = Number(input[0]);
-let d = [[]];
-d.push([0, 1]);
-for (let i = 2; i <= N; i++) {
-  d.push([]);
-  d[i][0] = BigInt(d[i - 1][0] + d[i - 1][1]);
-  d[i][1] = BigInt(d[i - 1][0]);
+const A = input[1].split(" ").map((num) => Number(num));
+let d = new Array(N).fill(1);
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < i; j++) {
+    if (A[j] < A[i] && d[j] >= d[i]) {
+      d[i] = d[j] + 1;
+    }
+  }
 }
 
-console.log((d[N][0] + d[N][1]).toString());
+console.log(d.reduce((p, c) => (p > c ? p : c)));
