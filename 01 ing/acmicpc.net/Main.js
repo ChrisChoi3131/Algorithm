d79677501f9a6dda1d30515abbb52e4ab8a0f0c0
Chris Choi <chrischoi3131@gmail.com>
2022-03-03 07:36:25 -0800

    https://www.acmicpc.net/problem/14002 DP, JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -10,12 +10,25 @@ let input = require("fs")
 const N = Number(input[0]);
 const A = input[1].split(" ").map((num) => Number(num));
 let d = new Array(N).fill(1);
+let m = new Array(N).fill(-1);
+
 for (let i = 0; i < N; i++) {
   for (let j = 0; j < i; j++) {
     if (A[j] < A[i] && d[j] >= d[i]) {
       d[i] = d[j] + 1;
+      m[i] = j;
     }
   }
 }
 
-console.log(d.reduce((p, c) => (p > c ? p : c)));
+let seq = [];
+let max = Math.max(...d);
+let maxIdx = d.indexOf(max);
+function recur(idx) {
+  if (idx === -1) return;
+  seq.unshift(A[idx]);
+  recur(m[idx]);
+}
+recur(maxIdx);
+console.log(max);
+console.log(seq.join(" "));
