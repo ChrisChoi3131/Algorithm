fe36f605ccb4462a425f3069c611a04ad1358aa1
Chris Choi <chrischoi3131@gmail.com>
2022-03-07 06:24:05 -0800

    https://www.acmicpc.net/problem/1699 DP, JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -8,12 +8,12 @@ let input = require("fs")
 // let input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
 
 const N = Number(input[0]);
-const A = input[1].split(" ").map((num) => Number(num));
 
-let d = [];
-
-d.push(A[0]);
-for (let i = 1; i < N; i++) {
-  d.push(Math.max(d[i - 1] + A[i], A[i]));
+let d = [0];
+for (let i = 1; i <= N; i++) {
+  d.push(Number.MAX_SAFE_INTEGER);
+  for (let j = 1; j * j <= i; j++) {
+    if (d[i] > d[i - j * j] + 1) d[i] = d[i - j * j] + 1;
+  }
 }
-console.log(Math.max(...d));
+console.log(d[N]);
