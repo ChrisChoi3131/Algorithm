bbc6ba5a499b3cc66754b27ca22aa7a09c3318fb
Chris Choi <chrischoi3131@gmail.com>
2022-03-08 00:06:52 -0800

    https://www.acmicpc.net/problem/2156 DP, JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -7,18 +7,18 @@ let input = require("fs")
   .split("\n");
 // let input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
 
-const T = Number(input[0]);
-let ans = [];
-for (let test_case = 1; test_case <= T; test_case++) {
-  const idx = (test_case - 1) * 3;
-  const N = Number(input[1 + idx]);
-  const A = [input[idx + 2].split(" ").map(Number), input[idx + 3].split(" ").map(Number)];
-  let d = [[A[0][0]], [A[1][0]], [0]];
-  for (let i = 1; i < N; i++) {
-    d[0][i] = Math.max(d[1][i - 1], d[2][i - 1]) + A[0][i];
-    d[1][i] = Math.max(d[0][i - 1], d[2][i - 1]) + A[1][i];
-    d[2][i] = Math.max(d[0][i - 1], d[1][i - 1], d[2][i - 1]);
-  }
-  ans.push(Math.max(d[0][N - 1], d[1][N - 1], d[2][N - 1]));
+const N = Number(input[0]);
+let a = [0];
+for (let i = 1; i <= N; i++) {
+  a.push(Number(input[i]));
 }
-console.log(ans.join("\n"));
+let d = [[0, 0, 0]];
+d.push([0, a[1], a[1]]);
+for (let i = 2; i <= N; i++) {
+  d.push([]);
+  d[i][0] = Math.max(...d[i - 1]);
+  d[i][1] = d[i - 1][0] + a[i];
+  d[i][2] = d[i - 1][1] + a[i];
+}
+
+console.log(Math.max(...d[N]));
