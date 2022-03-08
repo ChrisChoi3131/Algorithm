2cb86dde5df6995d6afcc42ed30dd7bb976adbcc
Chris Choi <chrischoi3131@gmail.com>
2022-03-07 16:16:00 -0800

    https://www.acmicpc.net/problem/9465 DP, JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -7,16 +7,18 @@ let input = require("fs")
   .split("\n");
 // let input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
 
-const N = Number(input[0]);
-const MOD = 10007;
-let d = [[], new Array(10).fill(1)];
-
-for (let i = 2; i <= N; i++) {
-  d.push(new Array(10).fill(0));
-  for (let j = 0; j <= 9; j++) {
-    for (let l = 0; l <= j; l++) {
-      d[i][j] += d[i - 1][l] % MOD;
-    }
+const T = Number(input[0]);
+let ans = [];
+for (let test_case = 1; test_case <= T; test_case++) {
+  const idx = (test_case - 1) * 3;
+  const N = Number(input[1 + idx]);
+  const A = [input[idx + 2].split(" ").map(Number), input[idx + 3].split(" ").map(Number)];
+  let d = [[A[0][0]], [A[1][0]], [0]];
+  for (let i = 1; i < N; i++) {
+    d[0][i] = Math.max(d[1][i - 1], d[2][i - 1]) + A[0][i];
+    d[1][i] = Math.max(d[0][i - 1], d[2][i - 1]) + A[1][i];
+    d[2][i] = Math.max(d[0][i - 1], d[1][i - 1], d[2][i - 1]);
   }
+  ans.push(Math.max(d[0][N - 1], d[1][N - 1], d[2][N - 1]));
 }
-console.log(d[N].reduce((p, c) => p + c) % MOD);
+console.log(ans.join("\n"));
