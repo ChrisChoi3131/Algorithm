9eff3827c7eadb067aece11c2ae6121f4480bf10
Chris Choi <chrischoi3131@gmail.com>
2022-03-09 16:10:23 -0800

    https://www.acmicpc.net/problem/11054 DP, JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -10,13 +10,25 @@ let input = require("fs")
 const N = Number(input[0]);
 const A = input[1].split(" ").map(Number);
 
-let d = new Array(N).fill(0);
-d[0] = 1;
-
+let forD = new Array(N).fill(0);
+let backD = new Array(N).fill(0);
+forD[0] = 1;
 for (let i = 1; i < N; i++) {
   for (let j = 0; j < i; j++) {
-    if (A[i] < A[j] && d[j] >= d[i]) d[i] = d[j];
+    if (A[i] > A[j] && forD[j] >= forD[i]) forD[i] = forD[j];
   }
-  d[i]++;
+  forD[i]++;
 }
-console.log(Math.max(...d));
+for (let i = N - 1; i >= 0; i--) {
+  for (let j = N - 1; j > i; j--) {
+    if (A[i] > A[j] && backD[j] > backD[i]) backD[i] = backD[j];
+  }
+  backD[i]++;
+}
+console.log(
+  Math.max(
+    ...forD.map((num, idx) => {
+      return num + backD[idx];
+    })
+  ) - 1
+);
