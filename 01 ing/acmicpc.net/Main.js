74e051be2b6ca8e7741e296cfbc6a58534b92099
Chris Choi <chrischoi3131@gmail.com>
2022-03-17 11:42:50 -0700

    https://www.acmicpc.net/problem/3085 DP, JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -6,23 +6,57 @@ let input = require("fs")
   .trim()
   .split("\n");
 // let input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
+const N = Number(input[0]);
+const CANDIES = ["P", "Z", "Y", "C"];
+let a = [];
 
-let heights = input.map(Number);
-let total = heights.reduce((p, c) => p + c);
-let indexs = [];
-outer: for (let i = 0; i < heights.length; i++) {
-  for (let j = 0; j < heights.length; j++) {
-    if (i !== j) {
-      let check = total - heights[i] - heights[j] === 100 ? true : false;
-      if (check) {
-        indexs.push(i, j);
-        break outer;
+const points = [
+  [-1, 0],
+  [0, -1],
+  [0, 1],
+  [1, 0],
+];
+for (let i = 1; i <= N; i++) {
+  a.push(input[i].split(""));
+}
+function findMaxNumber(array) {
+  let max = 0;
+  for (let i = 0; i < N; i++) {
+    let rowCount = 1;
+    let colCount = 1;
+    for (let j = 1; j < N; j++) {
+      if (array[i][j] === array[i][j - 1]) {
+        rowCount++;
+      } else {
+        rowCount = 1;
       }
+      if (max < rowCount) max = rowCount;
+
+      if (array[j][i] === array[j - 1][i]) {
+        colCount++;
+      } else {
+        colCount = 1;
+      }
+      if (max < colCount) max = colCount;
+    }
+  }
+  return max;
+}
+let maxNumber = 0;
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < N; j++) {
+    for (let k = 0; k < 4; k++) {
+      let x = i + points[k][0];
+      let y = j + points[k][1];
+      if (x < 0 || x >= N || y < 0 || y >= N) continue;
+      let copyA = JSON.parse(JSON.stringify(a));
+      let tmp = copyA[x][y];
+      copyA[x][y] = copyA[i][j];
+      copyA[i][j] = tmp;
+      let returnMaxNumber = findMaxNumber(copyA);
+      maxNumber < returnMaxNumber ? (maxNumber = returnMaxNumber) : "";
     }
   }
 }
-let ans = [];
-heights.forEach((ele, idx) => {
-  if (indexs.indexOf(idx) === -1) ans.push(ele);
-});
-console.log(ans.sort((a, b) => a - b).join("\n"));
+
+console.log(maxNumber);
