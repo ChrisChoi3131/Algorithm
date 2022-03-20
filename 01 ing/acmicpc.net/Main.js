3cdf40391fb86b0addaaa10896ab0163a7472d10
Chris Choi <chrischoi3131@gmail.com>
2022-03-19 20:51:02 -0700

    https://www.acmicpc.net/problem/6064 Brute Force, JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -1,145 +1,25 @@
 const inputFilePath = "/sample.txt";
 
-let input = require("fs")
+const input = require("fs")
   .readFileSync(__dirname + inputFilePath)
   .toString()
   .trim()
   .split("\n");
-// let input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
-const [n, m] = input[0].split(" ").map(Number);
-const tetrominos = [
-  [
-    { x: 0, y: 0 },
-    { x: 0, y: 1 },
-    { x: 0, y: 2 },
-    { x: 0, y: 3 },
-  ],
-  [
-    { x: 0, y: 0 },
-    { x: 1, y: 0 },
-    { x: 2, y: 0 },
-    { x: 3, y: 0 },
-  ],
-  [
-    { x: 0, y: 0 },
-    { x: 0, y: 1 },
-    { x: 1, y: 0 },
-    { x: 1, y: 1 },
-  ],
-  [
-    { x: 0, y: 0 },
-    { x: 1, y: 0 },
-    { x: 2, y: 0 },
-    { x: 2, y: 1 },
-  ],
-  [
-    { x: 0, y: 0 },
-    { x: 1, y: 0 },
-    { x: 0, y: 1 },
-    { x: 0, y: 2 },
-  ],
-  [
-    { x: 0, y: 0 },
-    { x: 0, y: 1 },
-    { x: 1, y: 1 },
-    { x: 2, y: 1 },
-  ],
-  [
-    { x: 0, y: 2 },
-    { x: 1, y: 0 },
-    { x: 1, y: 1 },
-    { x: 1, y: 2 },
-  ],
-  [
-    { x: 0, y: 1 },
-    { x: 1, y: 1 },
-    { x: 2, y: 1 },
-    { x: 2, y: 0 },
-  ],
-  [
-    { x: 0, y: 0 },
-    { x: 0, y: 1 },
-    { x: 0, y: 2 },
-    { x: 1, y: 2 },
-  ],
-  [
-    { x: 0, y: 0 },
-    { x: 0, y: 1 },
-    { x: 1, y: 0 },
-    { x: 2, y: 0 },
-  ],
-  [
-    { x: 0, y: 0 },
-    { x: 1, y: 0 },
-    { x: 1, y: 1 },
-    { x: 1, y: 2 },
-  ],
-  [
-    { x: 0, y: 0 },
-    { x: 1, y: 0 },
-    { x: 1, y: 1 },
-    { x: 2, y: 1 },
-  ],
-  [
-    { x: 0, y: 1 },
-    { x: 0, y: 2 },
-    { x: 1, y: 0 },
-    { x: 1, y: 1 },
-  ],
-  [
-    { x: 0, y: 1 },
-    { x: 1, y: 1 },
-    { x: 1, y: 0 },
-    { x: 2, y: 0 },
-  ],
-  [
-    { x: 0, y: 0 },
-    { x: 0, y: 1 },
-    { x: 1, y: 1 },
-    { x: 1, y: 2 },
-  ],
-  [
-    { x: 0, y: 1 },
-    { x: 1, y: 0 },
-    { x: 1, y: 1 },
-    { x: 1, y: 2 },
-  ],
-  [
-    { x: 0, y: 0 },
-    { x: 1, y: 0 },
-    { x: 1, y: 1 },
-    { x: 2, y: 0 },
-  ],
-  [
-    { x: 0, y: 0 },
-    { x: 0, y: 1 },
-    { x: 0, y: 2 },
-    { x: 1, y: 1 },
-  ],
-  [
-    { x: 0, y: 1 },
-    { x: 1, y: 0 },
-    { x: 1, y: 1 },
-    { x: 2, y: 1 },
-  ],
-];
-let arr = [];
-for (let i = 1; i <= n; i++) {
-  arr.push(input[i].split(" ").map(Number));
-}
-let max = 0;
-for (let i = 0; i < n; i++) {
-  for (let j = 0; j < m; j++) {
-    outer: for (let k = 0; k < tetrominos.length; k++) {
-      let sum = 0;
-      for (let l = 0; l < tetrominos[k].length; l++) {
-        const x = i + tetrominos[k][l].x;
-        const y = j + tetrominos[k][l].y;
-        if (x >= n || y >= m) continue outer;
-        sum += arr[x][y];
-      }
-      max < sum ? (max = sum) : "";
+// const input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
+const T = Number(input[0]);
+let ans = [];
+for (let test_case = 1; test_case <= T; test_case++) {
+  let [M, N, X, Y] = input[test_case].split(" ").map(Number);
+  let check = false;
+  X--;
+  Y--;
+  for (let i = X; i < N * M; i += M) {
+    if (i % N === Y) {
+      check = true;
+      ans.push(i + 1);
+      break;
     }
   }
+  if (!check) ans.push(-1);
 }
-console.log(max);
+console.log(ans.join("\n"));
