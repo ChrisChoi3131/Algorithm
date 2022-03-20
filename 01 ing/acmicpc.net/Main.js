137f68bc2ca393796de5e51944037a075a654d24
Chris Choi <chrischoi3131@gmail.com>
2022-03-20 15:32:46 -0700

    https://www.acmicpc.net/problem/1748 Brute Force, JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -6,20 +6,13 @@ const input = require("fs")
   .trim()
   .split("\n");
 // const input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
-const T = Number(input[0]);
-let ans = [];
-for (let test_case = 1; test_case <= T; test_case++) {
-  let [M, N, X, Y] = input[test_case].split(" ").map(Number);
-  let check = false;
-  X--;
-  Y--;
-  for (let i = X; i < N * M; i += M) {
-    if (i % N === Y) {
-      check = true;
-      ans.push(i + 1);
-      break;
-    }
-  }
-  if (!check) ans.push(-1);
+const N = Number(input[0]);
+const N_Len = N.toString().length - 1;
+let idx = 0,
+  length = 0;
+while (idx < N_Len) {
+  length += 9 * Math.pow(10, idx) * (idx + 1);
+  idx++;
 }
-console.log(ans.join("\n"));
+length += (N - Math.pow(10, N_Len) + 1) * (N_Len + 1);
+console.log(length);
