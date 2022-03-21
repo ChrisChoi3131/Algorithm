f6037dfa90b6695f800ba83fcb4b4f270f2104c9
Chris Choi <chrischoi3131@gmail.com>
2022-03-20 21:21:40 -0700

    https://www.acmicpc.net/problem/15651 Recursion, JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -11,18 +11,19 @@ const [n, m] = input[0].split(" ").map(Number);
 let visited = [],
   a = [],
   print = [];
-function go(idx, preNum) {
+
+function go(idx) {
   if (idx === m) {
     print.push(a.join(" "));
     return;
   }
-  for (let i = preNum; i <= n; i++) {
-    if (visited[i]) continue;
-    visited[i] = true;
+  for (let i = 1; i <= n; i++) {
+    // if (visited[i]) continue;
+    // visited[i] = true;
     a[idx] = i;
-    go(idx + 1, i);
-    visited[i] = false;
+    go(idx + 1);
+    // visited[i] = false;
   }
 }
-go(0, 1);
+go(0);
 console.log(print.join("\n"));
