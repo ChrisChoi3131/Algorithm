a9aefe2a612a6a3deb9956471722ff0d0112921c
Chris Choi <chrischoi3131@gmail.com>
2022-03-21 16:32:51 -0700

    https://www.acmicpc.net/problem/15663 Recursion, JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -8,22 +8,40 @@ const input = require("fs")
 // const input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
 const [n, m] = input[0].split(" ").map(Number);
 let arr = input[1].split(" ").map(Number);
-arr.sort((a, b) => a - b);
-arr.unshift(0);
-let visited = [],
-  a = [],
-  print = [];
+arr = countNums(arr);
+function countNums(arr) {
+  let nums = [],
+    count = [],
+    pre;
+  arr.sort((a, b) => a - b);
+  for (let i = 0; i < arr.length; i++) {
+    if (pre !== arr[i]) {
+      nums.push(arr[i]);
+      count.push(1);
+    } else {
+      count[count.length - 1]++;
+    }
+    pre = arr[i];
+  }
+  return [nums, count];
+}
+let c = arr[1];
+arr = arr[0];
+let line = [],
+  printArr = [];
 
-function go(idx, startIdx) {
+function go(idx) {
   if (idx === m) {
-    print.push(a.join(" "));
+    printArr.push(line.join(" "));
     return;
   }
-  for (let i = 1; i <= n; i++) {
-    if (arr[startIdx] > arr[i]) continue;
-    a[idx] = arr[i];
-    go(idx + 1, i);
+  for (let i = 0; i < arr.length; i++) {
+    if (c[i] === 0) continue;
+    c[i]--;
+    line[idx] = arr[i];
+    go(idx + 1);
+    c[i]++;
   }
 }
-go(0, 0);
-console.log(print.join("\n"));
+go(0);
+console.log(printArr.join("\n"));
