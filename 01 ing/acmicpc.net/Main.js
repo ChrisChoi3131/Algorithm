a5647fdf1c6048b95ac57fb5e5612223877027a0
Chris Choi <chrischoi3131@gmail.com>
2022-03-23 00:07:48 -0700

    https://www.acmicpc.net/problem/6603 Recursion, JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -6,70 +6,33 @@ const input = require("fs")
   .trim()
   .split("\n");
 // const input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
+let idx = 0;
+let arr = [];
+let c = [];
 
-const n = Number(input[0]);
-let w = [];
-for (let i = 1; i <= n; i++) {
-  w.push(input[i].split(" ").map(Number));
-}
-let currPerm = (function () {
-  let array = [];
-  for (let i = 0; i < n; i++) {
-    array.push(i);
-  }
-  return array;
-})();
-let numOfPerm = (function () {
-  let sum = 1;
-  for (let i = 1; i <= n; i++) {
-    sum *= i;
-  }
-  return sum;
-})();
+let ans = [];
+let line = [];
+const n = 6;
 
-function getDistance() {
-  let sumDis = 0;
-  let [start, end] = [currPerm[0], currPerm[n - 1]];
-  for (let i = 0; i < n - 1; i++) {
-    let [from, to] = [currPerm[i], currPerm[i + 1]];
-    if (w[from][to] !== 0) {
-      sumDis += w[from][to];
-    } else {
-      return 0;
-    }
+function go(idx) {
+  if (idx === n) {
+    ans.push(line.join(" ") + "\n");
+    return;
   }
-  return w[end][start] !== 0 ? sumDis + w[end][start] : 0;
-}
-function findNextPerm() {
-  let idx = n - 1;
-  for (let i = n - 1; i > 0; i--) {
-    if (currPerm[i - 1] < currPerm[i]) {
-      idx = i - 1;
-      break;
-    }
-  }
-  if (idx === n - 1) return false;
-  let idx2 = n - 1;
-  for (let i = n - 1; i > idx; i--) {
-    if (currPerm[idx] < currPerm[i]) {
-      idx2 = i;
-      break;
-    }
-  }
-  let tmp = currPerm[idx];
-  currPerm[idx] = currPerm[idx2];
-  currPerm[idx2] = tmp;
-  currPerm = currPerm.slice(0, idx + 1).concat(currPerm.slice(idx + 1).reverse());
-  return true;
-}
-let ans = getDistance() !== 0 ? getDistance() : Number.MAX_SAFE_INTEGER;
-for (let i = 0; i < numOfPerm; i++) {
-  if (findNextPerm()) {
-    if (currPerm[0] !== 0) break;
-    let dis = getDistance();
-    if (ans > dis && dis !== 0) ans = getDistance();
-  } else {
-    break;
+  for (let i = 0; i < arr.length; i++) {
+    if (c[i] || line[idx - 1] > arr[i]) continue;
+    c[i] = true;
+    line[idx] = arr[i];
+    go(idx + 1);
+    c[i] = false;
   }
 }
-console.log(ans);
+while (idx !== -1) {
+  arr = input[idx].split(" ").map(Number);
+  let k = arr.shift();
+  if (k === 0) break;
+  go(0);
+  idx++;
+  ans.push("\n");
+}
+console.log(ans.join(""));
