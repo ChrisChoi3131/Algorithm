06d45da9aa99bc3cd4d7bfd4f7aec2d312232fbb
Chris Choi <chrischoi3131@gmail.com>
2022-03-22 20:01:05 -0700

    https://www.acmicpc.net/problem/10974 Math, JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -6,30 +6,41 @@ const input = require("fs")
   .trim()
   .split("\n");
 // const input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
-let n = Number(input[0]);
-let currPerm = input[1].split(" ").map(Number);
 
-function findPrevPerm() {
+let n = Number(input[0]);
+let currPerm = new Array(n).fill(1).map((x, idx) => x + idx);
+
+let numOfPerms = (function () {
+  let sum = 1;
+  for (let i = 1; i <= n; i++) {
+    sum *= i;
+  }
+  return sum;
+})();
+
+function findNextPerm() {
   let idx = n - 1;
   for (let i = n - 1; i > 0; i--) {
-    if (currPerm[i - 1] > currPerm[i]) {
+    if (currPerm[i - 1] < currPerm[i]) {
       idx = i - 1;
       break;
     }
   }
-  if (idx === n - 1) return false;
-  currPerm = currPerm.slice(0, idx + 1).concat(currPerm.splice(idx + 1).reverse());
   let idx2 = n - 1;
-  for (let i = n - 1; i >= idx; i--) {
-    if (currPerm[idx] > currPerm[i]) {
+  for (let i = n - 1; i > idx; i--) {
+    if (currPerm[idx] < currPerm[i]) {
       idx2 = i;
+      break;
     }
   }
   let temp = currPerm[idx];
   currPerm[idx] = currPerm[idx2];
   currPerm[idx2] = temp;
-  return true;
+  currPerm = currPerm.slice(0, idx + 1).concat(currPerm.slice(idx + 1).reverse());
+  return currPerm.join(" ");
 }
-
-// splice slice compare
-findPrevPerm() ? console.log(currPerm.join(" ")) : console.log(-1);
+let ans = [currPerm.join(" ")];
+for (let i = 0; i < numOfPerms - 1; i++) {
+  ans.push(findNextPerm());
+}
+console.log(ans.join("\n"));
