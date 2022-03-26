fdaec101c16f7d0b9e2a401dc8347dab4c0149e8
Chris Choi <chrischoi3131@gmail.com>
2022-03-25 22:08:37 -0700

    https://www.acmicpc.net/problem/13023 Graph, JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -1,39 +1,34 @@
 const inputFilePath = "/sample.txt";
-
 const input = require("fs")
   .readFileSync(__dirname + inputFilePath)
   .toString()
   .trim()
   .split("\n");
 // const input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
-const [l, c] = input[0].split(" ").map(Number);
-let alphabets = input[1].split(" ").sort();
-let ans = [];
-let visited = [];
-let line = [];
-const vowels = ["a", "e", "i", "o", "u"];
-function go(idx, startIdx) {
-  if (idx === l) {
-    let numOfVowels = 0,
-      numOfConsonants = 0;
-    for (let i = 0; i < l; i++) {
-      if (vowels.includes(line[i])) {
-        numOfVowels++;
-      } else {
-        numOfConsonants++;
-      }
+
+const [n, m] = input[0].split(" ").map(Number);
+let adM = new Array(n).fill(0).map(() => new Array(n).fill(false));
+let adL = new Array(n).fill(0).map(() => new Array());
+let edgeL = [];
+for (let i = 1; i <= m; i++) {
+  const [a, b] = input[i].split(" ").map(Number);
+  adM[a][b] = adM[b][a] = true;
+  adL[a].push(b);
+  adL[b].push(a);
+  edgeL.push([a, b]);
+  edgeL.push([b, a]);
+}
+for (let i = 0; i < edgeL.length; i++) {
+  const [a, b] = edgeL[i];
+  for (let j = 0; j < edgeL.length; j++) {
+    const [c, d] = edgeL[j];
+    if (a === b || a === c || a === d || b === c || b === d || c === d) continue;
+    if (!adM[b][c]) continue;
+    for (const e of adL[d]) {
+      if (e === a || e === b || e === c || e === d) continue;
+      console.log(1);
+      process.exit();
     }
-    if (numOfVowels >= 1 && numOfConsonants >= 2) ans.push(line.join(""));
-    return;
-  }
-  for (let i = startIdx; i < c; i++) {
-    if (visited[i]) continue;
-    visited[i] = true;
-    line[idx] = alphabets[i];
-    go(idx + 1, i);
-    visited[i] = false;
   }
 }
-
-go(0, 0);
-console.log(ans.join("\n"));
+console.log(0);
