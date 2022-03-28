c24928a4bab66a7e73c9339eabda54cbd4cba27f
Chris Choi <chrischoi3131@gmail.com>
2022-03-28 14:18:12 -0700

    https://www.acmicpc.net/problem/2667 Graph, JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -5,40 +5,42 @@ const input = require("fs")
   .trim()
   .split("\n");
 // const input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
-
-let inputIdx = 0;
-const k = Number(input[inputIdx++]);
-let adL, check, isBiGraph;
-let ans = [];
-for (let test_case = 0; test_case < k; test_case++) {
-  const [v, e] = input[inputIdx++].split(" ").map(Number);
-  adL = new Array(v).fill(0).map(() => new Array());
-  check = new Array(v).fill(0);
-  isBiGraph = true;
-  for (let i = 0; i < e; i++) {
-    const [u, v] = input[inputIdx++].split(" ").map((num) => num - 1);
-    adL[u].push(v);
-    adL[v].push(u);
-  }
-  for (let i = 0; i < v; i++) {
-    if (check[i] === 0) dfs(i, 1);
-  }
-
-  if (!isBiGraph) ans.push("NO");
-  else if (isBiGraph) ans.push("YES");
+const n = Number(input[0]);
+const dx = [0, 0, 1, -1];
+const dy = [1, -1, 0, 0];
+let maps = [];
+let groups = new Array(n).fill(0).map(() => new Array(n).fill(0));
+for (let i = 1; i <= n; i++) {
+  maps.push(input[i].split("").map(Number));
 }
-console.log(ans.join("\n"));
-
-function dfs(v, groupIdx) {
-  if (check[v]) return;
-  check[v] = groupIdx;
-  for (const nextV of adL[v]) {
-    if (check[nextV] === 0) {
-      dfs(nextV, 3 - groupIdx);
-    } else {
-      if (check[v] === check[nextV]) {
-        isBiGraph = false;
+let groupIdx = 0;
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < n; j++) {
+    if (maps[i][j] === 1 && groups[i][j] === 0) {
+      let q = [];
+      groupIdx++;
+      q.unshift({ x: i, y: j });
+      groups[i][j] = groupIdx;
+      while (q.length) {
+        let { x, y } = q.pop();
+        for (let k = 0; k < 4; k++) {
+          let nextX = x + dx[k];
+          let nextY = y + dy[k];
+          if (nextX < 0 || nextY < 0 || nextX >= n || nextY >= n) continue;
+          if (maps[nextX][nextY] === 1 && groups[nextX][nextY] === 0) {
+            q.unshift({ x: nextX, y: nextY });
+            groups[nextX][nextY] = groupIdx;
+          }
+        }
       }
     }
   }
 }
+let ans = new Array(groupIdx).fill(0);
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < n; j++) {
+    if (groups[i][j] !== 0) ans[groups[i][j] - 1]++;
+  }
+}
+console.log(groupIdx);
+console.log(ans.sort((a, b) => a - b).join("\n"));
