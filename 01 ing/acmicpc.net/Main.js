c934ee4ca234a7fbdd899db2ab8ba31f9b4d7a72
Chris Choi <chrischoi3131@gmail.com>
2022-03-28 15:48:44 -0700

    https://www.acmicpc.net/problem/2178 Graph-BFS, JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -6,44 +6,26 @@ const input = require("fs")
   .split("\n");
 // const input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
 
-let w, h;
-let map, check;
-let groupIdx;
-let inputIdx = 0;
-let ans = [];
-const dx = [0, 0, -1, 1, -1, -1, 1, 1];
-const dy = [-1, 1, 0, 0, -1, 1, -1, 1];
-do {
-  [w, h] = input[inputIdx++].split(" ").map(Number);
-  if (w === 0 && h === 0) break;
-  groupIdx = 0;
-  map = [];
-  check = new Array(h).fill(0).map(() => new Array(w).fill(0));
-  for (let i = 0; i < h; i++) {
-    map.push(input[inputIdx++].split(" ").map(Number));
-  }
-
-  for (let i = 0; i < h; i++) {
-    for (let j = 0; j < w; j++) {
-      if (check[i][j] === 0 && map[i][j] === 1) {
-        groupIdx++;
-        dfs(i, j);
-      }
-    }
-  }
-  ans.push(groupIdx + "\n");
-} while (true);
-
-console.log(ans.join(""));
-function dfs(x, y) {
-  if (check[x][y] !== 0) return;
-  check[x][y] = groupIdx;
-  for (let i = 0; i < 8; i++) {
+const [n, m] = input[0].split(" ").map(Number);
+const dx = [0, 0, -1, 1];
+const dy = [-1, 1, 0, 0];
+let map = [];
+let check = new Array(n).fill(0).map(() => new Array(m).fill(Number.MAX_SAFE_INTEGER));
+for (let i = 1; i <= n; i++) {
+  map.push(input[i].split("").map(Number));
+}
+let q = [{ x: 0, y: 0 }];
+check[0][0] = 1;
+while (q.length) {
+  const { x, y } = q.pop();
+  for (let i = 0; i < 4; i++) {
     const nx = x + dx[i];
     const ny = y + dy[i];
-    if (nx < 0 || ny < 0 || nx >= h || ny >= w) continue;
-    if (map[nx][ny] === 1 && check[nx][ny] === 0) {
-      dfs(nx, ny);
+    if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+    if (check[nx][ny] > check[x][y] + 1 && map[nx][ny] === 1) {
+      check[nx][ny] = check[x][y] + 1;
+      q.unshift({ x: nx, y: ny });
     }
   }
 }
+console.log(check[n - 1][m - 1]);
