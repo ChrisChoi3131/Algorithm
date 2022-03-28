1604007f14730740e7c6fe4b0253bdb1d3f855f4
Chris Choi <chrischoi3131@gmail.com>
2022-03-28 15:31:02 -0700

    https://www.acmicpc.net/problem/4963 Graph, JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -5,42 +5,45 @@ const input = require("fs")
   .trim()
   .split("\n");
 // const input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
-const n = Number(input[0]);
-const dx = [0, 0, 1, -1];
-const dy = [1, -1, 0, 0];
-let maps = [];
-let groups = new Array(n).fill(0).map(() => new Array(n).fill(0));
-for (let i = 1; i <= n; i++) {
-  maps.push(input[i].split("").map(Number));
-}
-let groupIdx = 0;
-for (let i = 0; i < n; i++) {
-  for (let j = 0; j < n; j++) {
-    if (maps[i][j] === 1 && groups[i][j] === 0) {
-      let q = [];
-      groupIdx++;
-      q.unshift({ x: i, y: j });
-      groups[i][j] = groupIdx;
-      while (q.length) {
-        let { x, y } = q.pop();
-        for (let k = 0; k < 4; k++) {
-          let nextX = x + dx[k];
-          let nextY = y + dy[k];
-          if (nextX < 0 || nextY < 0 || nextX >= n || nextY >= n) continue;
-          if (maps[nextX][nextY] === 1 && groups[nextX][nextY] === 0) {
-            q.unshift({ x: nextX, y: nextY });
-            groups[nextX][nextY] = groupIdx;
-          }
-        }
+
+let w, h;
+let map, check;
+let groupIdx;
+let inputIdx = 0;
+let ans = [];
+const dx = [0, 0, -1, 1, -1, -1, 1, 1];
+const dy = [-1, 1, 0, 0, -1, 1, -1, 1];
+do {
+  [w, h] = input[inputIdx++].split(" ").map(Number);
+  if (w === 0 && h === 0) break;
+  groupIdx = 0;
+  map = [];
+  check = new Array(h).fill(0).map(() => new Array(w).fill(0));
+  for (let i = 0; i < h; i++) {
+    map.push(input[inputIdx++].split(" ").map(Number));
+  }
+
+  for (let i = 0; i < h; i++) {
+    for (let j = 0; j < w; j++) {
+      if (check[i][j] === 0 && map[i][j] === 1) {
+        groupIdx++;
+        dfs(i, j);
       }
     }
   }
-}
-let ans = new Array(groupIdx).fill(0);
-for (let i = 0; i < n; i++) {
-  for (let j = 0; j < n; j++) {
-    if (groups[i][j] !== 0) ans[groups[i][j] - 1]++;
+  ans.push(groupIdx + "\n");
+} while (true);
+
+console.log(ans.join(""));
+function dfs(x, y) {
+  if (check[x][y] !== 0) return;
+  check[x][y] = groupIdx;
+  for (let i = 0; i < 8; i++) {
+    const nx = x + dx[i];
+    const ny = y + dy[i];
+    if (nx < 0 || ny < 0 || nx >= h || ny >= w) continue;
+    if (map[nx][ny] === 1 && check[nx][ny] === 0) {
+      dfs(nx, ny);
+    }
   }
 }
-console.log(groupIdx);
-console.log(ans.sort((a, b) => a - b).join("\n"));
