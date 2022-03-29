cc757219f787d089bb3c4c7133336b0de25a7785
Chris Choi <chrischoi3131@gmail.com>
2022-03-28 23:06:04 -0700

    https://www.acmicpc.net/problem/16929 Graph-DFS, JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -5,45 +5,38 @@ const input = require("fs")
   .trim()
   .split("\n");
 // const input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
-const n = Number(input[0]);
 
-let q;
-let head = 0,
-  tail = 0;
-
-const q_push = (point) => (q[tail++] = point);
-const q_pop = () => q[head++];
-const q_size = () => tail - head;
-
-const dx = [-2, -2, -1, -1, 1, 1, 2, 2];
-const dy = [-1, 1, -2, 2, -2, 2, -1, 1];
-let ans = [];
-for (let i = 1; i <= n * 3; i += 3) {
-  const l = Number(input[i]);
-  (head = 0), (tail = 0);
-
-  let check = new Array(l).fill(0).map(() => new Array(l).fill(0));
-  const [sx, sy] = input[i + 1].split(" ").map(Number);
-  const [ex, ey] = input[i + 2].split(" ").map(Number);
-  if (sx === ex && sy === ey) {
-    ans.push(0);
-    continue;
-  }
-  q = [];
-  q_push({ x: sx, y: sy });
-  while (q_size()) {
-    let { x, y } = q_pop();
-    for (let i = 0; i < 8; i++) {
-      let nx = x + dx[i];
-      let ny = y + dy[i];
-      if (nx < 0 || ny < 0 || nx >= l || ny >= l) continue;
-      if (check[nx][ny] === 0) {
-        check[nx][ny] = check[x][y] + 1;
-        if (nx === ex && ny === ey) break;
-        q_push({ x: nx, y: ny });
+const [n, m] = input[0].split(" ").map(Number);
+const dx = [-1, 1, 0, 0];
+const dy = [0, 0, -1, 1];
+let playGround = [];
+let check = new Array(n).fill(0).map(() => new Array(m).fill(false));
+for (let i = 1; i <= n; i++) {
+  playGround.push(input[i].split(""));
+}
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < m; j++) {
+    if (!check[i][j]) {
+      let result = dfs(i, j, -1, -1, playGround[i][j]);
+      if (result) {
+        console.log("Yes");
+        process.exit();
       }
     }
   }
-  ans.push(check[ex][ey]);
 }
-console.log(ans.join("\n"));
+console.log("No");
+function dfs(x, y, px, py, color) {
+  if (check[x][y]) return true;
+  check[x][y] = true;
+  for (let i = 0; i < 4; i++) {
+    const nx = x + dx[i];
+    const ny = y + dy[i];
+    if (px === nx && py === ny) continue;
+    if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+    if (playGround[nx][ny] === color) {
+      if (dfs(nx, ny, x, y, color)) return true;
+    }
+  }
+  return false;
+}
