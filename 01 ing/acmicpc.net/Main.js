58b597d35c4f57538a8eaa93b3cc1957f8630a10
Chris Choi <chrischoi3131@gmail.com>
2022-03-28 18:50:15 -0700

    https://www.acmicpc.net/problem/7576 Graph, JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -6,26 +6,67 @@ const input = require("fs")
   .split("\n");
 // const input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
 
-const [n, m] = input[0].split(" ").map(Number);
+const [m, n] = input[0].split(" ").map(Number);
+const [unripeTomato, ripeTomato] = [0, 1];
 const dx = [0, 0, -1, 1];
 const dy = [-1, 1, 0, 0];
-let map = [];
-let check = new Array(n).fill(0).map(() => new Array(m).fill(Number.MAX_SAFE_INTEGER));
+let head = 0,
+  tail = 0;
+let q = new Array();
+const q_push = (dat) => {
+  q[tail++] = dat;
+};
+const q_pop = () => {
+  return q[head++];
+};
+const q_size = () => tail - head;
+
+let tomatoes = [];
+let timeToRipe = new Array(n).fill(0).map(() => new Array(m).fill(0));
+let isExistUnripeToamto = false;
+let isExistRipeToamto = false;
 for (let i = 1; i <= n; i++) {
-  map.push(input[i].split("").map(Number));
+  const row = input[i].split(" ").map(Number);
+  row.indexOf(unripeTomato) !== -1 ? (isExistUnripeToamto = true) : "";
+  row.indexOf(ripeTomato) !== -1 ? (isExistRipeToamto = true) : "";
+  tomatoes.push(row);
 }
-let q = [{ x: 0, y: 0 }];
-check[0][0] = 1;
-while (q.length) {
-  const { x, y } = q.pop();
-  for (let i = 0; i < 4; i++) {
-    const nx = x + dx[i];
-    const ny = y + dy[i];
-    if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-    if (check[nx][ny] > check[x][y] + 1 && map[nx][ny] === 1) {
-      check[nx][ny] = check[x][y] + 1;
-      q.unshift({ x: nx, y: ny });
+if (isExistRipeToamto && !isExistUnripeToamto) {
+  console.log(0);
+} else if (!isExistRipeToamto) {
+  console.log(-1);
+} else {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      if (tomatoes[i][j] === ripeTomato) {
+        q_push({ x: i, y: j });
+      }
     }
   }
+  let maxTime = 0;
+  while (q_size()) {
+    const { x, y } = q_pop();
+    for (let i = 0; i < 4; i++) {
+      const nx = x + dx[i];
+      const ny = y + dy[i];
+      if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+      if (tomatoes[nx][ny] === unripeTomato) {
+        if (timeToRipe[nx][ny] === 0) {
+          timeToRipe[nx][ny] = timeToRipe[x][y] + 1;
+          maxTime < timeToRipe[nx][ny] ? (maxTime = timeToRipe[nx][ny]) : "";
+          q_push({ x: nx, y: ny });
+        }
+      }
+    }
+  }
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      if (timeToRipe[i][j] === 0 && tomatoes[i][j] === unripeTomato) {
+        console.log(-1);
+        process.exit();
+      }
+    }
+  }
+
+  console.log(maxTime);
 }
-console.log(check[n - 1][m - 1]);
