f4aa5ae343e4cc74ef480eb1b58395a346c6beed
Chris Choi <chrischoi3131@gmail.com>
2022-03-30 23:30:36 -0700

    https://www.acmicpc.net/submit/10808 BF, JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -5,12 +5,12 @@ const input = require("fs")
   .split("\n");
 // const input = require("fs").readFileSync('/dev/stdin').toString().split("\n");
 
-let cntAlphabet = new Array(26).fill(-1);
+let cntAlphabet = new Array(26).fill(0);
 let chars = input[0].split("");
 let idxSmallLetter = 97;
 
 chars.forEach((char) => {
-  cntAlphabet[char.charCodeAt() - idxSmallLetter] = chars.indexOf(char);
+  cntAlphabet[char.charCodeAt() - idxSmallLetter]++;
 });
 
 console.log(cntAlphabet.join(" "));
