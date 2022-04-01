d933bb0b09abd1e9a7efb18605d76c8967c41f2a
Chris Choi <chrischoi3131@gmail.com>
2022-03-31 21:59:12 -0700

    https://www.acmicpc.net/problem/2225 DP, JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -5,12 +5,16 @@ const input = require("fs")
   .split("\n");
 // const input = require("fs").readFileSync('/dev/stdin').toString().split("\n");
 
-let cntAlphabet = new Array(26).fill(0);
-let chars = input[0].split("");
-let idxSmallLetter = 97;
-
-chars.forEach((char) => {
-  cntAlphabet[char.charCodeAt() - idxSmallLetter]++;
-});
-
-console.log(cntAlphabet.join(" "));
+const [n, k] = input[0].split(" ").map(Number);
+let d = new Array(201).fill(0).map(() => new Array(201).fill(0));
+const mod = 1000000000;
+d[1] = d[1].map((ele, idx) => idx);
+for (let i = 0; i < d.length; i++) {
+  d[i][1] = 1;
+}
+for (let i = 2; i < d.length; i++) {
+  for (let j = 2; j < d[i].length; j++) {
+    d[i][j] = (d[i - 1][j] + d[i][j - 1]) % mod;
+  }
+}
+console.log(d[n][k]);
