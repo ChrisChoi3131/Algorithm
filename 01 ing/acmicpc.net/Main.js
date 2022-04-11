512a29ad4687ec2ad1a586804242acdb06ca0402
Chris Choi <chrischoi3131@gmail.com>
2022-04-11 12:46:47 -0700

    https://www.acmicpc.net/problem/1697 BFS, JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -4,47 +4,41 @@ const input = require("fs")
   .toString()
   .split("\n");
 // const input = require("fs").readFileSync('/dev/stdin').toString().split("\n");
-const n = Number(input[0]);
-input[1] = input[1].split("");
-const matrix = new Array(n).fill(0).map(() => new Array(n));
-let idx = 0;
-const ans = [];
-for (let i = 0; i < n; i++) {
-  for (let j = i; j < n; j++) {
-    matrix[i][j] = input[1][idx++];
-  }
-}
 
-go(0);
-console.log(ans.join(" "));
-function go(index) {
-  if (index === n) return true;
-  if (matrix[index][index] === "0") {
-    ans[index] = 0;
-    return checkAns(index) && go(index + 1);
-  }
-  for (let i = 1; i <= 10; i++) {
-    if (matrix[index][index] === "+") {
-      ans[index] = i;
-    } else if (matrix[index][index] === "-") {
-      ans[index] = -i;
-    }
-    if (checkAns(index) && go(index + 1)) return true;
-  }
-  return false;
-}
+const [n, k] = input[0].split(" ").map(Number);
+const time = new Array(100000 + 1).fill(0);
+const q = [];
+const q_push = (ele) => {
+  q[tail++] = ele;
+};
+const q_pop = () => {
+  return q[head++];
+};
+const q_size = () => {
+  return tail - head;
+};
 
-function checkAns(index) {
-  let sum = 0;
-  for (let i = index; i >= 0; i--) {
-    sum += ans[i];
-    if (matrix[i][index] === "0") {
-      if (sum !== 0) return false;
-    } else if (matrix[i][index] === "+") {
-      if (sum <= 0) return false;
-    } else if (matrix[i][index] === "-") {
-      if (sum >= 0) return false;
+let head = 0,
+  tail = 0;
+const nextPoints = (point) => {
+  let nextPoints = [];
+  if (point + 1 <= 100000) nextPoints.push(point + 1);
+  if (point - 1 >= 0) nextPoints.push(point - 1);
+  if (2 * point <= 100000) nextPoints.push(2 * point);
+  return nextPoints;
+};
+
+q_push(n);
+time[n] = 0;
+while (q_size()) {
+  const currPoint = q_pop();
+  if (currPoint === k) break;
+  const currTime = time[currPoint];
+  nextPoints(currPoint).forEach((nextPoint) => {
+    if (!time[nextPoint]) {
+      q_push(nextPoint);
+      time[nextPoint] = currTime + 1;
     }
-  }
-  return true;
+  });
 }
+console.log(time[k]);
