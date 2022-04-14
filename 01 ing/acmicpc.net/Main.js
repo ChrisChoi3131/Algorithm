9fa06cdbd90227620a680d11680de591b3b6d187
Chris Choi <chrischoi3131@gmail.com>
2022-04-14 15:35:58 -0700

    https://www.acmicpc.net/problem/16940 Graph-BFS, JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -4,53 +4,53 @@ const input = require("fs")
   .toString()
   .split("\n");
 // const input = require("fs").readFileSync('/dev/stdin').toString().split("\n");
-const [m, n] = input[0].split(" ").map(Number);
-const matrix = [];
-const dx = [-1, 1, 0, 0];
-const dy = [0, 0, -1, 1];
-for (let i = 1; i <= n; i++) {
-  matrix.push(input[i].split("").map(Number));
+
+const n = Number(input[0]);
+const route = input[n].split(" ").map((num) => Number(num) - 1);
+const edges = new Array(n).fill(0).map(() => new Array());
+const check = new Array(n).fill(false);
+for (let i = 1; i <= n - 1; i++) {
+  const [x, y] = input[i].split(" ").map(Number);
+  edges[x - 1].push(y - 1);
+  edges[y - 1].push(x - 1);
 }
-const cnt = new Array(n).fill(0).map(() => new Array(m).fill(-1));
+const q = [];
+let head = 0,
+  tail = 0;
+const q_push = (vertex) => (q[tail++] = vertex);
+const q_pop = () => q[head++];
+const q_size = () => tail - head;
 
-const qEmptyRoom = [];
-const qWall = [];
-qEmptyRoom.unshift([0, 0]);
-cnt[0][0] = 0;
+if (route[0] !== 0) {
+  console.log(0);
+  process.exit();
+}
 
-while (qEmptyRoom.length || qWall.length) {
-  while (qEmptyRoom.length) {
-    const [cx, cy] = qEmptyRoom.pop();
-    for (let i = 0; i < 4; i++) {
-      const [nx, ny] = [cx + dx[i], cy + dy[i]];
-      if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
-        if (cnt[nx][ny] === -1 && matrix[nx][ny] === 0) {
-          qEmptyRoom.unshift([nx, ny]);
-          cnt[nx][ny] = cnt[cx][cy];
-        } else if (cnt[nx][ny] === -1 && matrix[nx][ny] === 1) {
-          qWall.unshift([nx, ny]);
-          cnt[nx][ny] = cnt[cx][cy] + 1;
-        }
+q_push(route[0]);
+check[route[0]] = true;
+let from = 1,
+  to = 1;
+while (q_size()) {
+  const cv = q_pop();
+  to = from;
+  edges[cv].forEach((vertex) => (!check[vertex] ? to++ : null));
+  let pushCnt = 0;
+  for (let i = from; i < to; i++) {
+    const nv = route[i];
+    if (!check[nv]) {
+      if (edges[cv].indexOf(nv) !== -1) {
+        q_push(nv);
+        check[nv] = true;
+        pushCnt++;
+      } else {
+        console.log(0);
+        process.exit();
       }
+    } else {
+      console.log(0);
+      process.exit();
     }
   }
-  while (qWall.length) {
-    const [cx, cy] = qWall.pop();
-    let check = false;
-    for (let i = 0; i < 4; i++) {
-      const [nx, ny] = [cx + dx[i], cy + dy[i]];
-      if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
-        if (cnt[nx][ny] === -1 && matrix[nx][ny] === 0) {
-          qEmptyRoom.unshift([nx, ny]);
-          cnt[nx][ny] = cnt[cx][cy];
-          check = true;
-        } else if (cnt[nx][ny] === -1 && matrix[nx][ny] === 1) {
-          qWall.unshift([nx, ny]);
-          cnt[nx][ny] = cnt[cx][cy] + 1;
-        }
-      }
-    }
-    if (check) break;
-  }
+  from += pushCnt;
 }
-console.log(cnt[n - 1][m - 1]);
+console.log(1);
