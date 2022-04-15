a8944772e127628a9227a4a2851bbd7c6694e11e
Chris Choi <chrischoi3131@gmail.com>
2022-04-15 13:22:01 -0700

    https://www.acmicpc.net/problem/16964 Graph-DFS, JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -9,48 +9,45 @@ const n = Number(input[0]);
 const route = input[n].split(" ").map((num) => Number(num) - 1);
 const edges = new Array(n).fill(0).map(() => new Array());
 const check = new Array(n).fill(false);
+const parent = new Array(n).fill(-1);
 for (let i = 1; i <= n - 1; i++) {
   const [x, y] = input[i].split(" ").map(Number);
   edges[x - 1].push(y - 1);
   edges[y - 1].push(x - 1);
 }
-const q = [];
-let head = 0,
-  tail = 0;
-const q_push = (vertex) => (q[tail++] = vertex);
-const q_pop = () => q[head++];
-const q_size = () => tail - head;
-
 if (route[0] !== 0) {
   console.log(0);
   process.exit();
 }
-
-q_push(route[0]);
-check[route[0]] = true;
-let from = 1,
-  to = 1;
-while (q_size()) {
-  const cv = q_pop();
-  to = from;
-  edges[cv].forEach((vertex) => (!check[vertex] ? to++ : null));
-  let pushCnt = 0;
-  for (let i = from; i < to; i++) {
-    const nv = route[i];
-    if (!check[nv]) {
-      if (edges[cv].indexOf(nv) !== -1) {
-        q_push(nv);
-        check[nv] = true;
-        pushCnt++;
-      } else {
-        console.log(0);
-        process.exit();
-      }
-    } else {
+check[0] = true;
+edges[0].forEach((child) => {
+  parent[child] = 0;
+});
+for (let i = 1; i < n; i++) {
+  if (route[i - 1] === parent[route[i]]) {
+    edges[route[i]].forEach((child) => {
+      parent[child] = route[i];
+    });
+    check[route[i]] = true;
+    continue;
+  }
+  let cnt = 0;
+  edges[route[i - 1]].forEach((child) => {
+    if (!check[child]) cnt++;
+  });
+  if (cnt) {
+    console.log(0);
+    process.exit();
+  } else {
+    if (!check[parent[route[i]]]) {
       console.log(0);
       process.exit();
+    } else {
+      edges[route[i]].forEach((child) => {
+        parent[child] = route[i];
+      });
+      check[route[i]] = true;
     }
   }
-  from += pushCnt;
 }
 console.log(1);
