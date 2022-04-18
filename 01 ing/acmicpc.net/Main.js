d83aa9c94ce4c894a27b3131338001835df3b11b
Chris Choi <chrischoi3131@gmail.com>
2022-04-18 14:26:19 -0700

    https://www.acmicpc.net/problem/1167 Graph-Tree, DFS:JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -7,26 +7,38 @@ const input = require("fs")
 // const input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
 const n = Number(input[0]);
 const tree = new Array(n).fill(0).map(() => new Array());
-const parent = new Array(n).fill(-1);
-input.splice(1, n).map((line) => {
-  const [x, y] = line.split(" ").map((node) => Number(node) - 1);
-  tree[x].push(y);
-  tree[y].push(x);
-});
+let distance = new Array(n).fill(0);
+let root = 0;
+let maxDis = 0;
+for (let i = 1; i <= n; i++) {
+  const line = input[i].split(" ").map(Number);
+  const x = line[0] - 1;
+  for (let j = 1; j < line.length; j += 2) {
+    const [y, dis] = [line[j] - 1, line[j + 1]];
+    tree[x].push({ node: y, dis });
+    tree[y].push({ node: x, dis });
+    if (line[j + 2] === -1) break;
+  }
+}
+dfs(root, 0);
 
-dfs(0, 0);
-
-function dfs(node, parentNode) {
-  parent[node] = parentNode;
-  tree[node].forEach((child) => {
-    if (parent[child] === -1) {
-      dfs(child, node);
+function dfs(from, sumDistance) {
+  tree[from].forEach((to) => {
+    const { dis, node } = to;
+    if (node !== root) {
+      if (distance[node] === 0) {
+        distance[node] = sumDistance + dis;
+        dfs(node, distance[node]);
+      }
     }
   });
 }
-
-const ans = [];
-for (let i = 1; i < n; i++) {
-  ans.push(parent[i] + 1);
+for (let i = 0; i < n; i++) {
+  if (maxDis < distance[i]) {
+    maxDis = distance[i];
+    root = i;
+  }
 }
-console.log(ans.join("\n"));
+distance = new Array(n).fill(0);
+dfs(root, 0);
+console.log(Math.max(...distance));
