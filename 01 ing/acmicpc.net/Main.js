99220a1cda4329106f18570408584bf56e96d893
Chris Choi <chrischoi3131@gmail.com>
2022-04-18 14:49:28 -0700

    https://www.acmicpc.net/problem/1967 Graph-Tree,DFS:JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -6,39 +6,31 @@ const input = require("fs")
   .split("\n");
 // const input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
 const n = Number(input[0]);
-const tree = new Array(n).fill(0).map(() => new Array());
-let distance = new Array(n).fill(0);
-let root = 0;
-let maxDis = 0;
-for (let i = 1; i <= n; i++) {
-  const line = input[i].split(" ").map(Number);
-  const x = line[0] - 1;
-  for (let j = 1; j < line.length; j += 2) {
-    const [y, dis] = [line[j] - 1, line[j + 1]];
-    tree[x].push({ node: y, dis });
-    tree[y].push({ node: x, dis });
-    if (line[j + 2] === -1) break;
-  }
-}
-dfs(root, 0);
-
-function dfs(from, sumDistance) {
-  tree[from].forEach((to) => {
-    const { dis, node } = to;
-    if (node !== root) {
-      if (distance[node] === 0) {
-        distance[node] = sumDistance + dis;
-        dfs(node, distance[node]);
-      }
+const tree = new Array(n + 1).fill(0).map(() => new Array());
+input.splice(1, n).forEach((line) => {
+  const [x, y, dis] = line.split(" ").map(Number);
+  tree[x].push({ to: y, dis });
+  tree[y].push({ to: x, dis });
+});
+let root = 1;
+let distance = new Array(n + 1).fill(0);
+dfs(root);
+function dfs(from) {
+  tree[from].forEach((node) => {
+    const { to, dis } = node;
+    if (distance[to] === 0 && to !== root) {
+      distance[to] = distance[from] + dis;
+      dfs(to);
     }
   });
 }
-for (let i = 0; i < n; i++) {
+let maxDis = 0;
+for (let i = 1; i <= n; i++) {
   if (maxDis < distance[i]) {
-    maxDis = distance[i];
     root = i;
+    maxDis = distance[i];
   }
 }
-distance = new Array(n).fill(0);
-dfs(root, 0);
+distance = new Array(n + 1).fill(0);
+dfs(root);
 console.log(Math.max(...distance));
