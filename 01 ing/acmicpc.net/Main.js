8412815d021c34b092c5c135c4579083238d5e4c
Chris Choi <chrischoi3131@gmail.com>
2022-04-18 00:07:19 -0700

    https://www.acmicpc.net/problem/2250 Graph-Tree, JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -4,43 +4,52 @@ const input = require("fs")
   .toString()
   .split("\n");
 // const input = require("fs").readFileSync('/dev/stdin').toString().split("\n");
-
 const n = Number(input[0]);
-const tree = new Array(n);
-input
-  .splice(1, n + 1)
-  .map((line) => line.trim().split(" "))
-  .forEach((line) => {
-    const [root, left, right] = line.map((node) => node.charCodeAt() - 65);
-    tree[root] = { left, right };
-  });
+const tree = new Array(n + 1);
+const left = new Array(n + 1).fill(0);
+const right = new Array(n + 1).fill(0);
+const cnt = new Array(n + 1).fill(0);
+input.splice(1, n + 1).map((line) => {
+  const [root, left, right] = line.split(" ").map((node) => (node !== "-1" ? Number(node) : -1));
+  tree[root] = { left, right };
+  if (left !== -1) cnt[left]++;
+  if (right !== -1) cnt[right]++;
+});
 
-const ans = [];
-preOrder(0);
-ans.push("\n");
-inOrder(0);
-ans.push("\n");
-postOrder(0);
-ans.push("\n");
-console.log(ans.join(""));
-function preOrder(node) {
-  ans.push(String.fromCharCode(node + 65));
-  const { left, right } = tree[node];
-  if (left >= 0) preOrder(left);
-  if (right >= 0) preOrder(right);
+let order = 0;
+let root = 0;
+for (let i = 1; i <= n; i++) {
+  if (cnt[i] === 0) root = i;
 }
 
-function inOrder(node) {
-  const { left, right } = tree[node];
+inOrder(root, 1);
 
-  if (left >= 0) inOrder(left);
-  ans.push(String.fromCharCode(node + 65));
-  if (right >= 0) inOrder(right);
+function inOrder(node, depth) {
+  if (node === -1) return;
+  const { left, right } = tree[node];
+  inOrder(left, depth + 1);
+  tree[node].depth = depth;
+  tree[node].order = ++order;
+  inOrder(right, depth + 1);
 }
 
-function postOrder(node) {
-  const { left, right } = tree[node];
-  if (left >= 0) postOrder(left);
-  if (right >= 0) postOrder(right);
-  ans.push(String.fromCharCode(node + 65));
+let maxDepth = 0;
+for (let i = 1; i <= n; i++) {
+  const { depth, order } = tree[i];
+  if (left[depth] === 0) {
+    left[depth] = order;
+  } else {
+    left[depth] = Math.min(left[depth], order);
+  }
+  right[depth] = Math.max(right[depth], order);
+  maxDepth = Math.max(maxDepth, depth);
 }
+let maxWidth = 0;
+let depthOfMax = 0;
+for (let depth = 1; depth <= maxDepth; depth++) {
+  if (maxWidth < right[depth] - left[depth] + 1) {
+    maxWidth = right[depth] - left[depth] + 1;
+    depthOfMax = depth;
+  }
+}
+console.log(depthOfMax, maxWidth);
