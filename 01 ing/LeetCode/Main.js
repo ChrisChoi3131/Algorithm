95fba491a5770f0f532a5394363f43fc1085e3a3
Chris Choi <chrischoi3131@gmail.com>
2022-04-25 14:29:12 -0700

    https://leetcode.com/problems/range-addition/ Array-Prefix Sum:JS

--- 01 ing/LeetCode/Main.js
+++ 01 ing/LeetCode/Main.js
@@ -1,57 +1,26 @@
 /**
- * @param {number} n
+ * @param {number} length
+ * @param {number[][]} updates
+ * @return {number[]}
  */
-class TicTacToe {
-  constructor(n) {
-    this.n = n;
-    this.rows = new Array(n).fill(0);
-    this.cols = new Array(n).fill(0);
-    this.diags = 0;
-    this.antiDiags = 0;
+const getModifiedArray = function (length, updates) {
+  const arr = new Array(length + 1).fill(0);
+  updates.forEach(update => {
+    const [idxStart, idxEnd, inc] = update;
+    arr[idxStart] += inc;
+    arr[idxEnd + 1] -= inc;
+  });
+  for (let i = 1; i < arr.length; i++) {
+    arr[i] = arr[i] + arr[i - 1];
   }
-}
-
-/**
- * @param {number} row
- * @param {number} col
- * @param {number} player
- * @return {number}
- */
-TicTacToe.prototype.move = function (row, col, player) {
-  let dir = 1;
-  player === 2 ? (dir = -1) : null;
-  this.rows[row] += dir;
-  this.cols[col] += dir;
-  if (row === col) this.diags += dir;
-  if (row + col === this.n - 1) this.antiDiags += dir;
-  const conditionsWin = [
-    Math.abs(this.rows[row]) === this.n,
-    Math.abs(this.cols[col]) === this.n,
-    Math.abs(this.diags) === this.n,
-    Math.abs(this.antiDiags) === this.n,
-  ];
-  if (conditionsWin.includes(true)) return player;
-  else return 0;
+  return arr.splice(0, length);
 };
 
-const n = 3;
+const length = 10,
+  updates = [
+    [2, 4, 6],
+    [5, 6, 8],
+    [1, 9, -4],
+  ];
 
-const input = [
-  [0, 0, 1],
-  [0, 2, 2],
-  [2, 2, 1],
-  [1, 1, 2],
-  [2, 0, 1],
-  [1, 0, 2],
-  [2, 1, 1],
-];
-const obj = new TicTacToe(n);
-for (let i = 0; i < input.length; i++) {
-  console.log(obj.move(...input[i]));
-}
-console.log();
-/**
- * Your TicTacToe object will be instantiated and called as such:
- * var obj = new TicTacToe(n)
- * var param_1 = obj.move(row,col,player)
- */
+console.log(getModifiedArray(length, updates));
