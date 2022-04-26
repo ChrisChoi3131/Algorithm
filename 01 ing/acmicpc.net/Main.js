1a1154570a8bbb0b26060e5f8126b6c8b6a6ddb7
Chris Choi <chrischoi3131@gmail.com>
2022-04-26 13:24:31 -0700

    https://www.acmicpc.net/problem/14889 Bitmask:JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -5,16 +5,32 @@ const input = require('fs')
   .trim()
   .split('\n');
 // const input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
-
-const [n, s] = input[0].split(' ').map(Number);
-const arr = input[1].split(' ').map(Number);
-
-let cnt = 0;
-for (let i = 1; i < 1 << n; i++) {
-  let sum = 0;
-  for (let j = 0; j < n; j++) {
-    if (i & (1 << j)) sum += arr[j];
-  }
-  if (s === sum) cnt++;
+const n = Number(input[0]);
+const abilities = [];
+for (let i = 1; i <= n; i++) {
+  abilities.push(input[i].split(' ').map(Number));
 }
-console.log(cnt);
+
+let ans = Infinity;
+for (let i = 1; i < 1 << n; i++) {
+  const first = [];
+  const second = [];
+
+  for (let j = 0; j < n; j++) {
+    if (i & (1 << j)) first.push(j);
+    else second.push(j);
+  }
+  if (first.length !== n / 2) continue;
+
+  let abilitiesFirst = 0;
+  let abilitiesSecond = 0;
+  for (let j = 0; j < n / 2; j++) {
+    for (let k = 0; k < n / 2; k++) {
+      abilitiesFirst += abilities[first[j]][first[k]];
+      abilitiesSecond += abilities[second[j]][second[k]];
+    }
+  }
+  const abilityDiff = Math.abs(abilitiesFirst - abilitiesSecond);
+  if (ans > Math.abs(abilitiesFirst - abilitiesSecond)) ans = abilityDiff;
+}
+console.log(ans);
