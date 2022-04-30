32cf3d2848a55d7651fd25b2b6136abf9720fb28
Chris Choi <chrischoi3131@gmail.com>
2022-04-30 07:29:45 -0700

    https://www.acmicpc.net/problem/1182 Recursion:JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -6,35 +6,17 @@ const input = require('fs')
   .split('\n');
 // const input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
 
-const ans = [];
-let check = [];
-let arr = [];
-let map = {};
-for (let i = 0; i < input.length - 1; i++) {
-  map = {};
-  const s = input[i].split(' ').map(Number).splice(1, input[i].split(' ').length);
-  s.forEach((element, idx) => {
-    map[idx] = element;
-  });
-  check = new Array(s.length).fill(false);
-  arr = [];
-  go(s, 0, 0);
-  console.log('');
-}
-
-function go(s, idx, start) {
-  if (idx === 6) {
-    const line = [];
-    arr.map(idx => line.push(map[idx]));
-    console.log(line.join(' '));
+const [n, s] = input[0].split(' ').map(Number);
+const nums = input[1].split(' ').map(Number);
+let cnt = 0;
+go(0, 0);
+function go(idx, sum) {
+  if (idx === n) {
+    if (sum === s) cnt++;
     return;
   }
-  for (let i = 0; i < s.length; i++) {
-    if (check[i]) continue;
-    if (arr[idx - 1] > i) continue;
-    check[i] = true;
-    arr[idx] = i;
-    go(s, idx + 1);
-    check[i] = false;
-  }
+  go(idx + 1, sum + nums[idx]);
+  go(idx + 1, sum);
 }
+s === 0 ? cnt-- : null;
+console.log(cnt);
