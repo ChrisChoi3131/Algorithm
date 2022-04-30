3c84e524d1b82ca73e584e19fe96c4f38b349b2b
Chris Choi <chrischoi3131@gmail.com>
2022-04-30 07:44:41 -0700

    https://www.acmicpc.net/problem/14225 Recursion:JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -9,14 +9,20 @@ const input = require('fs')
 const [n, s] = input[0].split(' ').map(Number);
 const nums = input[1].split(' ').map(Number);
 let cnt = 0;
+const sums = {};
 go(0, 0);
 function go(idx, sum) {
   if (idx === n) {
-    if (sum === s) cnt++;
+    sums[sum] = 1;
     return;
   }
   go(idx + 1, sum + nums[idx]);
   go(idx + 1, sum);
 }
-s === 0 ? cnt-- : null;
-console.log(cnt);
+
+for (let i = 1; i < Infinity; i++) {
+  if (!sums[i]) {
+    console.log(i);
+    break;
+  }
+}
