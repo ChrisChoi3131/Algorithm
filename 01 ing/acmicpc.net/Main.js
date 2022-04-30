59751cabed2662a4a62dfc8f8d32382d8383b1c7
Chris Choi <chrischoi3131@gmail.com>
2022-04-30 11:30:19 -0700

    https://www.acmicpc.net/problem/15658 Recursion:JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -4,25 +4,42 @@ const input = require('fs')
   .toString()
   .trim()
   .split('\n');
-// const input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
-
-const [n, s] = input[0].split(' ').map(Number);
+//const input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
+const n = Number(input[0]);
 const nums = input[1].split(' ').map(Number);
-let cnt = 0;
-const sums = {};
-go(0, 0);
-function go(idx, sum) {
-  if (idx === n) {
-    sums[sum] = 1;
+const maxOperators = input[2].split(' ').map(Number);
+const mapOperators = { 0: '+', 1: '-', 2: '*', 3: '/' };
+const cntOperators = [0, 0, 0, 0];
+const operators = [];
+let maxNum = -Infinity;
+let minNum = Infinity;
+go(0);
+function go(idx) {
+  if (idx === n - 1) {
+    let num = nums[0];
+    for (let i = 0; i < nums.length - 1; i++) {
+      const operator = mapOperators[operators[i]];
+      operator === '+' ? (num = num + nums[i + 1]) : null;
+      operator === '-' ? (num = num - nums[i + 1]) : null;
+      operator === '*' ? (num = num * nums[i + 1]) : null;
+      if (operator === '/') {
+        const rem = num % nums[i + 1];
+        num = (num - rem) / nums[i + 1];
+      }
+    }
+    maxNum < num ? (maxNum = num) : null;
+    minNum > num ? (minNum = num) : null;
     return;
   }
-  go(idx + 1, sum + nums[idx]);
-  go(idx + 1, sum);
-}
 
-for (let i = 1; i < Infinity; i++) {
-  if (!sums[i]) {
-    console.log(i);
-    break;
+  for (const operator in mapOperators) {
+    if (cntOperators[operator] < maxOperators[operator]) {
+      cntOperators[operator]++;
+      operators[idx] = operator;
+      go(idx + 1);
+      cntOperators[operator]--;
+    }
   }
 }
+console.log(maxNum);
+console.log(minNum);
