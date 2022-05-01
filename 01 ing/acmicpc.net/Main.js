3c745e2a8a720b97aa1574d77fc3e1ec6b075182
Chris Choi <chrischoi3131@gmail.com>
2022-04-30 20:52:42 -0700

    https://www.acmicpc.net/problem/11279 Data Structure-Heap:JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -5,43 +5,44 @@ const input = require('fs')
   .trim()
   .split('\n');
 //const input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
-
-const [wall, space, coin] = ['#', '.', 'o'];
-const dx = [-1, 1, 0, 0];
-const dy = [0, 0, -1, 1];
-const [n, m] = input[0].split(' ').map(Number);
-const board = input.splice(1, n + 1).map(ele => ele.split(''));
-const coins = [];
-for (let i = 0; i < n; i++) {
-  for (let j = 0; j < m; j++) {
-    if (board[i][j] === coin) coins.push([i, j]);
+const n = Number(input[0]);
+const heap = new Array(n + 1).fill(-Infinity);
+let idxH = 0;
+const h_push = num => {
+  heap[++idxH] = num;
+  for (let i = idxH; i > 1; i = Math.floor(i / 2)) {
+    if (heap[i] > heap[Math.floor(i / 2)]) {
+      const tmp = heap[Math.floor(i / 2)];
+      heap[Math.floor(i / 2)] = heap[i];
+      heap[i] = tmp;
+    } else break;
   }
-}
-
-console.log(go(0, ...coins[0], ...coins[1]));
-
-function go(idx, x1, y1, x2, y2) {
-  if (idx === 11) return -1;
-  let [isDropFirst, isDropSecond] = [false, false];
-  if (x1 < 0 || y1 < 0 || x1 >= n || y1 >= m) isDropFirst = true;
-  if (x2 < 0 || y2 < 0 || x2 >= n || y2 >= m) isDropSecond = true;
-  if (isDropFirst && isDropSecond) return -1;
-  if (isDropFirst || isDropSecond) return idx;
-
-  let ans = -1;
-  for (let i = 0; i < 4; i++) {
-    let [nx1, ny1, nx2, ny2] = [x1 + dx[i], y1 + dy[i], x2 + dx[i], y2 + dy[i]];
-    if (nx1 >= 0 && ny1 >= 0 && nx1 < n && ny1 < m && board[nx1][ny1] === wall) {
-      nx1 = x1;
-      ny1 = y1;
+};
+const h_pop = () => {
+  if (idxH === 0) return 0;
+  const num = heap[1];
+  heap[1] = heap[idxH];
+  heap[idxH--] = -Infinity;
+  for (let i = 1; i * 2 <= idxH; ) {
+    if (heap[i] > heap[i * 2] && heap[i] > heap[i * 2 + 1]) break;
+    else if (heap[i * 2] > heap[i * 2 + 1]) {
+      const tmp = heap[i];
+      heap[i] = heap[i * 2];
+      heap[i * 2] = tmp;
+      i = i * 2;
+    } else {
+      const tmp = heap[i];
+      heap[i] = heap[i * 2 + 1];
+      heap[i * 2 + 1] = tmp;
+      i = i * 2 + 1;
     }
-    if (nx2 >= 0 && ny2 >= 0 && nx2 < n && ny2 < m && board[nx2][ny2] === wall) {
-      nx2 = x2;
-      ny2 = y2;
-    }
-    const res = go(idx + 1, nx1, ny1, nx2, ny2);
-    if (res === -1) continue;
-    if (ans === -1 || ans > res) ans = res;
   }
-  return ans;
+  return num;
+};
+let ans = '';
+for (let i = 1; i <= n; i++) {
+  const x = Number(input[i]);
+  if (x === 0) ans += h_pop() + '\n';
+  else h_push(x);
 }
+console.log(ans);
