ca5ebd0f596b97b84fc3965502aacd00b92a0836
Chris Choi <chrischoi3131@gmail.com>
2022-05-01 10:02:08 -0700

    https://www.acmicpc.net/problem/1655 Data Structure-Heap:JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -6,56 +6,69 @@ const input = require('fs')
   .split('\n');
 //const input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
 const n = Number(input[0]);
+const heapMax = new Array(n + 1).fill(-Infinity);
+const heapMin = new Array(n + 1).fill(Infinity);
+let idxHeapMax = 0;
+let idxHeapMin = 0;
 
-const heap = new Array(n + 1).fill(Infinity);
-let idxHeap = 0;
-
-const h_push = num => {
-  heap[++idxHeap] = num;
-  for (let i = idxHeap; i > 1; i = Math.floor(i / 2)) {
-    if (Math.abs(heap[i]) < Math.abs(heap[Math.floor(i / 2)])) {
-      const tmp = heap[Math.floor(i / 2)];
-      heap[Math.floor(i / 2)] = heap[i];
-      heap[i] = tmp;
-    } else if (Math.abs(heap[i]) === Math.abs(heap[Math.floor(i / 2)])) {
-      if (heap[i] < heap[Math.floor(i / 2)]) {
-        const tmp = heap[Math.floor(i / 2)];
-        heap[Math.floor(i / 2)] = heap[i];
-        heap[i] = tmp;
-      } else break;
+const hmax_push = num => {
+  heapMax[++idxHeapMax] = num;
+  for (let i = idxHeapMax; i > 1; i = Math.floor(i / 2)) {
+    if (heapMax[i] > heapMax[Math.floor(i / 2)]) {
+      const tmp = heapMax[Math.floor(i / 2)];
+      heapMax[Math.floor(i / 2)] = heapMax[i];
+      heapMax[i] = tmp;
+    } else break;
+  }
+};
+const hmin_push = num => {
+  heapMin[++idxHeapMin] = num;
+  for (let i = idxHeapMin; i > 1; i = Math.floor(i / 2)) {
+    if (heapMin[i] < heapMin[Math.floor(i / 2)]) {
+      const tmp = heapMin[Math.floor(i / 2)];
+      heapMin[Math.floor(i / 2)] = heapMin[i];
+      heapMin[i] = tmp;
     } else break;
   }
 };
 
-const h_pop = () => {
-  if (idxHeap === 0) return 0;
-  const num = heap[1];
-  heap[1] = heap[idxHeap];
-  heap[idxHeap--] = Infinity;
-  for (let i = 1; i * 2 <= idxHeap; ) {
-    if (Math.abs(heap[i]) < Math.abs(heap[i * 2]) && Math.abs(heap[i]) < Math.abs(heap[i * 2 + 1])) break;
-    else if (Math.abs(heap[i * 2]) < Math.abs(heap[i * 2 + 1])) {
-      const tmp = heap[i];
-      heap[i] = heap[i * 2];
-      heap[i * 2] = tmp;
+const hmax_pop = () => {
+  const num = heapMax[1];
+  heapMax[1] = heapMax[idxHeapMax];
+  heapMax[idxHeapMax--] = -Infinity;
+  for (let i = 1; i * 2 <= idxHeapMax; ) {
+    if (heapMax[i] > heapMax[i * 2] && heapMax[i] > heapMax[i * 2 + 1]) break;
+    else if (heapMax[i * 2] > heapMax[i * 2 + 1]) {
+      const tmp = heapMax[i];
+      heapMax[i] = heapMax[i * 2];
+      heapMax[i * 2] = tmp;
       i = i * 2;
-    } else if (Math.abs(heap[i * 2]) > Math.abs(heap[i * 2 + 1])) {
-      const tmp = heap[i];
-      heap[i] = heap[i * 2 + 1];
-      heap[i * 2 + 1] = tmp;
-      i = i * 2 + 1;
     } else {
-      if (heap[i * 2] < heap[i * 2 + 1]) {
-        const tmp = heap[i];
-        heap[i] = heap[i * 2];
-        heap[i * 2] = tmp;
-        i = i * 2;
-      } else {
-        const tmp = heap[i];
-        heap[i] = heap[i * 2 + 1];
-        heap[i * 2 + 1] = tmp;
-        i = i * 2 + 1;
-      }
+      const tmp = heapMax[i];
+      heapMax[i] = heapMax[i * 2 + 1];
+      heapMax[i * 2 + 1] = tmp;
+      i = i * 2 + 1;
+    }
+  }
+  return num;
+};
+
+const hmin_pop = () => {
+  const num = heapMin[1];
+  heapMin[1] = heapMin[idxHeapMin];
+  heapMin[idxHeapMin--] = Infinity;
+  for (let i = 1; i * 2 <= idxHeapMin; ) {
+    if (heapMin[i] < heapMin[i * 2] && heapMin[i] < heapMin[i * 2 + 1]) break;
+    else if (heapMin[i * 2] < heapMin[i * 2 + 1]) {
+      const tmp = heapMin[i];
+      heapMin[i] = heapMin[i * 2];
+      heapMin[i * 2] = tmp;
+      i = i * 2;
+    } else {
+      const tmp = heapMin[i];
+      heapMin[i] = heapMin[i * 2 + 1];
+      heapMin[i * 2 + 1] = tmp;
+      i = i * 2 + 1;
     }
   }
   return num;
@@ -63,8 +76,24 @@ const h_pop = () => {
 
 let ans = '';
 for (let i = 1; i <= n; i++) {
-  const x = Number(input[i]);
-  if (x === 0) ans += h_pop() + '\n';
-  else h_push(x);
+  const num = Number(input[i]);
+  if (num < heapMin[1]) hmax_push(num);
+  else hmin_push(num);
+  if (Math.abs(idxHeapMax - idxHeapMin) > 1) {
+    if (idxHeapMax > idxHeapMin) {
+      hmin_push(hmax_pop());
+    } else {
+      hmax_push(hmin_pop());
+    }
+  }
+  if (i % 2 === 1) {
+    if (idxHeapMax > idxHeapMin) {
+      ans += heapMax[1] + '\n';
+    } else {
+      ans += heapMin[1] + '\n';
+    }
+  } else {
+    ans += Math.min(heapMax[1], heapMin[1]) + '\n';
+  }
 }
 console.log(ans);
