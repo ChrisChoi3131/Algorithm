c2c2b63a78ef04465e19a22a5bb504462d0cfdb1
Chris Choi <chrischoi3131@gmail.com>
2022-05-01 12:44:57 -0700

    https://www.acmicpc.net/problem/16198 Recursion:JS

--- 01 ing/acmicpc.net/Main.js
+++ 01 ing/acmicpc.net/Main.js
@@ -6,94 +6,41 @@ const input = require('fs')
   .split('\n');
 //const input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
 const n = Number(input[0]);
-const heapMax = new Array(n + 1).fill(-Infinity);
-const heapMin = new Array(n + 1).fill(Infinity);
-let idxHeapMax = 0;
-let idxHeapMin = 0;
+const visited = new Array(n).fill(false);
+const energies = input[1].split(' ').map(Number);
+let orders = [];
+let ans = 0;
+go(0);
 
-const hmax_push = num => {
-  heapMax[++idxHeapMax] = num;
-  for (let i = idxHeapMax; i > 1; i = Math.floor(i / 2)) {
-    if (heapMax[i] > heapMax[Math.floor(i / 2)]) {
-      const tmp = heapMax[Math.floor(i / 2)];
-      heapMax[Math.floor(i / 2)] = heapMax[i];
-      heapMax[i] = tmp;
-    } else break;
-  }
-};
-const hmin_push = num => {
-  heapMin[++idxHeapMin] = num;
-  for (let i = idxHeapMin; i > 1; i = Math.floor(i / 2)) {
-    if (heapMin[i] < heapMin[Math.floor(i / 2)]) {
-      const tmp = heapMin[Math.floor(i / 2)];
-      heapMin[Math.floor(i / 2)] = heapMin[i];
-      heapMin[i] = tmp;
-    } else break;
-  }
-};
-
-const hmax_pop = () => {
-  const num = heapMax[1];
-  heapMax[1] = heapMax[idxHeapMax];
-  heapMax[idxHeapMax--] = -Infinity;
-  for (let i = 1; i * 2 <= idxHeapMax; ) {
-    if (heapMax[i] > heapMax[i * 2] && heapMax[i] > heapMax[i * 2 + 1]) break;
-    else if (heapMax[i * 2] > heapMax[i * 2 + 1]) {
-      const tmp = heapMax[i];
-      heapMax[i] = heapMax[i * 2];
-      heapMax[i * 2] = tmp;
-      i = i * 2;
-    } else {
-      const tmp = heapMax[i];
-      heapMax[i] = heapMax[i * 2 + 1];
-      heapMax[i * 2 + 1] = tmp;
-      i = i * 2 + 1;
+function go(idx) {
+  if (idx === n - 2) {
+    const check = new Array(n).fill(false);
+    let energy = 0;
+    for (let i = 0; i < orders.length; i++) {
+      check[orders[i]] = true;
+      let [left, right] = [orders[i], orders[i]];
+      for (let j = orders[i] - 1; j >= 0; j--) {
+        if (check[j] === false) {
+          left = j;
+          break;
+        }
+      }
+      for (let j = orders[i] + 1; j < n; j++) {
+        if (check[j] === false) {
+          right = j;
+          break;
+        }
+      }
+      energy += energies[left] * energies[right];
     }
+    ans < energy ? (ans = energy) : null;
   }
-  return num;
-};
-
-const hmin_pop = () => {
-  const num = heapMin[1];
-  heapMin[1] = heapMin[idxHeapMin];
-  heapMin[idxHeapMin--] = Infinity;
-  for (let i = 1; i * 2 <= idxHeapMin; ) {
-    if (heapMin[i] < heapMin[i * 2] && heapMin[i] < heapMin[i * 2 + 1]) break;
-    else if (heapMin[i * 2] < heapMin[i * 2 + 1]) {
-      const tmp = heapMin[i];
-      heapMin[i] = heapMin[i * 2];
-      heapMin[i * 2] = tmp;
-      i = i * 2;
-    } else {
-      const tmp = heapMin[i];
-      heapMin[i] = heapMin[i * 2 + 1];
-      heapMin[i * 2 + 1] = tmp;
-      i = i * 2 + 1;
-    }
-  }
-  return num;
-};
-
-let ans = '';
-for (let i = 1; i <= n; i++) {
-  const num = Number(input[i]);
-  if (num < heapMin[1]) hmax_push(num);
-  else hmin_push(num);
-  if (Math.abs(idxHeapMax - idxHeapMin) > 1) {
-    if (idxHeapMax > idxHeapMin) {
-      hmin_push(hmax_pop());
-    } else {
-      hmax_push(hmin_pop());
-    }
-  }
-  if (i % 2 === 1) {
-    if (idxHeapMax > idxHeapMin) {
-      ans += heapMax[1] + '\n';
-    } else {
-      ans += heapMin[1] + '\n';
-    }
-  } else {
-    ans += Math.min(heapMax[1], heapMin[1]) + '\n';
+  for (let i = 1; i < n - 1; i++) {
+    if (visited[i]) continue;
+    orders[idx] = i;
+    visited[i] = true;
+    go(idx + 1);
+    visited[i] = false;
   }
 }
 console.log(ans);
