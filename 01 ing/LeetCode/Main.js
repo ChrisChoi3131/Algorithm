93b315ab36e78468cc5f6a0d77a14a263917b758
Chris Choi <chrischoi3131@gmail.com>
2022-05-03 10:50:53 -0700

    https://leetcode.com/problems/design-hashset/ Data Structure-Hashtable

--- 01 ing/LeetCode/Main.js
+++ 01 ing/LeetCode/Main.js
@@ -1,66 +1,37 @@
+class MyHashSet {
+  constructor() {
+    this.set = new Map();
+  }
+}
+
 /**
- * @param {number[][]} points
- * @param {number} k
- * @return {number[][]}
+ * @param {number} key
+ * @return {void}
  */
-
-const points = [
-    [3, 3],
-    [5, -1],
-    [-2, 4],
-  ],
-  k = 2;
-
-const kClosest = function (points, k) {
-  const heap = new Array(10001).fill([0, 0, Infinity]);
-  let idxHeap = 0;
-  const h_push = point => {
-    heap[++idxHeap] = point;
-    for (let i = idxHeap; i > 1; i = Math.floor(i / 2)) {
-      const [, , pdis] = heap[Math.floor(i / 2)];
-      const [, , cdis] = heap[i];
-      if (cdis < pdis) {
-        const tmp = heap[Math.floor(i / 2)];
-        heap[Math.floor(i / 2)] = heap[i];
-        heap[i] = tmp;
-      } else break;
-    }
-  };
-  const h_pop = () => {
-    const point = heap[1];
-    heap[1] = heap[idxHeap];
-    heap[idxHeap] = [0, 0, Infinity];
-
-    for (let i = 1; i * 2 <= idxHeap; ) {
-      const dis = heap[i][2];
-      const lDis = heap[i * 2][2];
-      const rDis = heap[i * 2 + 1][2];
-      if (dis < lDis && dis < rDis) break;
-      else if (lDis < rDis) {
-        const tmp = heap[i];
-        heap[i] = heap[i * 2];
-        heap[i * 2] = tmp;
-        i = i * 2;
-      } else {
-        const tmp = heap[i];
-        heap[i] = heap[i * 2 + 1];
-        heap[i * 2 + 1] = tmp;
-        i = i * 2 + 1;
-      }
-    }
-    return [point[0], point[1]];
-  };
-
-  for (let i = 0; i < points.length; i++) {
-    const [x, y] = points[i];
-    const dis = Math.pow(x, 2) + Math.pow(y, 2);
-    h_push([x, y, dis]);
-  }
-  const ans = [];
-  for (let i = 0; i < k; i++) {
-    ans.push(h_pop());
-  }
-  return ans;
+MyHashSet.prototype.add = function (key) {
+  this.set.set(key, key);
 };
 
-console.log(kClosest(points, k));
+/**
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.remove = function (key) {
+  this.set.delete(key);
+};
+
+/**
+ * @param {number} key
+ * @return {boolean}
+ */
+MyHashSet.prototype.contains = function (key) {
+  return this.set.has(key);
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * var obj = new MyHashSet()
+ * obj.add(key)
+ * obj.remove(key)
+ * var param_3 = obj.contains(key)
+ */
