44fd8fdffbd8982fcb673e52fb1883cf36522696
Chris Choi <chrischoi3131@gmail.com>
2022-05-03 20:21:28 -0700

    https://leetcode.com/problems/first-unique-character-in-a-string/submissions/ Data Structure-Hashtable:JS

--- 01 ing/LeetCode/Main.js
+++ 01 ing/LeetCode/Main.js
@@ -1,25 +1,13 @@
-/**
- * @param {string[]} list1
- * @param {string[]} list2
- * @return {string[]}
- */
-const list1 = ['Shogun', 'Tapioca Express', 'Burger King', 'KFC'],
-  list2 = ['KFC', 'Shogun', 'Burger King'];
-// const list1 = ['Shogun', 'Tapioca Express', 'Burger King', 'KFC'],
-//   list2 = ['KFC', 'Burger King', 'Tapioca Express', 'Shogun'];
-const findRestaurant = function (list1, list2) {
-  const map1 = new Map(list1.map((menu, idx) => [menu, idx]));
-  const map2 = new Map(list2.map((menu, idx) => [menu, idx]));
-  const res = [];
-  for (const list of map1) {
-    const [menu, idx] = list;
-    if (map2.get(menu) !== undefined) {
-      const sumIdx = idx + map2.get(menu);
-      res[sumIdx] === undefined ? (res[sumIdx] = []) : null;
-      res[sumIdx].push(menu);
-    }
+const firstUniqChar = function (s) {
+  const sSplited = s.split('');
+  const map = new Map();
+  for (const char of sSplited) {
+    if (map.has(char)) map.set(char, map.get(char) + 1);
+    else map.set(char, 1);
   }
-  for (const commonMenu of res) if (commonMenu) return commonMenu;
+  for (let i = 0; i < sSplited.length; i++) {
+    const char = sSplited[i];
+    if (map.get(char) === 1) return i;
+  }
+  return -1;
 };
-
-console.log(findRestaurant(list1, list2));
