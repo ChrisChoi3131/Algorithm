4cdd3276777a55ecf7a9127005856b44c206cf37
Chris Choi <chrischoi3131@gmail.com>
2022-05-03 20:43:39 -0700

    https://leetcode.com/problems/intersection-of-two-arrays-ii/ Data Structure-Hashtable:JS

--- 01 ing/LeetCode/Main.js
+++ 01 ing/LeetCode/Main.js
@@ -1,13 +1,30 @@
-const firstUniqChar = function (s) {
-  const sSplited = s.split('');
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+const nums1 = [1, 2, 2, 1],
+  nums2 = [2, 2];
+// const nums1 = [4, 9, 5],
+//   nums2 = [9, 4, 9, 8, 4];
+// const nums1 = [1, 2, 2, 1],
+//   nums2 = [2, 2];
+
+const intersect = function (nums1, nums2) {
   const map = new Map();
-  for (const char of sSplited) {
-    if (map.has(char)) map.set(char, map.get(char) + 1);
-    else map.set(char, 1);
-  }
-  for (let i = 0; i < sSplited.length; i++) {
-    const char = sSplited[i];
-    if (map.get(char) === 1) return i;
-  }
-  return -1;
+
+  const ans = [];
+  nums1.forEach(num => {
+    if (map.has(num)) map.set(num, map.get(num) + 1);
+    else map.set(num, 1);
+  });
+  nums2.forEach(num => {
+    if (map.has(num) && map.get(num) > 0) {
+      map.set(num, map.get(num) - 1);
+      ans.push(num);
+    }
+  });
+  return [...ans];
 };
+
+console.log(intersect(nums1, nums2));
