ec0f27db101008d0b16d93728567157c9d3e622b
Chris Choi <chrischoi3131@gmail.com>
2022-05-03 20:57:32 -0700

    https://leetcode.com/problems/contains-duplicate-ii/submissions/ Data Structure-Hashtable:JS

--- 01 ing/LeetCode/Main.js
+++ 01 ing/LeetCode/Main.js
@@ -1,30 +1,20 @@
 /**
- * @param {number[]} nums1
- * @param {number[]} nums2
- * @return {number[]}
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
  */
-const nums1 = [1, 2, 2, 1],
-  nums2 = [2, 2];
-// const nums1 = [4, 9, 5],
-//   nums2 = [9, 4, 9, 8, 4];
-// const nums1 = [1, 2, 2, 1],
-//   nums2 = [2, 2];
+// const nums = [1, 2, 3, 1, 2, 3],
+//   k = 2;
+const nums = [1, 2, 3, 1],
+  k = 3;
 
-const intersect = function (nums1, nums2) {
+const containsNearbyDuplicate = function (nums, k) {
   const map = new Map();
-
-  const ans = [];
-  nums1.forEach(num => {
-    if (map.has(num)) map.set(num, map.get(num) + 1);
-    else map.set(num, 1);
-  });
-  nums2.forEach(num => {
-    if (map.has(num) && map.get(num) > 0) {
-      map.set(num, map.get(num) - 1);
-      ans.push(num);
-    }
-  });
-  return [...ans];
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(nums[i]) && Math.abs(map.get(nums[i]) - i) <= k) {
+      return true;
+    } else map.set(nums[i], i);
+  }
+  return false;
 };
-
-console.log(intersect(nums1, nums2));
+console.log(containsNearbyDuplicate(nums, k));
