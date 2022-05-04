a1dbb3b91e940fa1a30b7111351faaf74cca1b2d
Chris Choi <chrischoi3131@gmail.com>
2022-05-04 06:07:16 -0700

    https://leetcode.com/problems/group-anagrams/solution/ Data Strructure-Hashtable,DesignedKey:JS

--- 01 ing/LeetCode/Main.js
+++ 01 ing/LeetCode/Main.js
@@ -1,22 +1,29 @@
-class Logger {
-  constructor() {
-    this.map = new Map();
-  }
-}
+const strs = [''];
+// const strs = ['eat', 'tea', 'tan', 'ate', 'nat', 'bat'];
 
-/**
- * @param {number} timestamp
- * @param {string} message
- * @return {boolean}
- */
-Logger.prototype.shouldPrintMessage = function (timestamp, message) {
-  if (this.map.get(message) === undefined) {
-    this.map.set(message, timestamp + 10);
-    return true;
-  } else {
-    if (timestamp >= this.map.get(message)) {
-      this.map.set(message, timestamp + 10);
-      return true;
-    } else return false;
+const generateKey = function (str) {
+  const strSplited = str.split('');
+  const cntAlphabet = new Array(26).fill(0);
+  for (let i = 0; i < strSplited.length; i++) {
+    const idxAlphabet = strSplited[i].charCodeAt() - 97;
+    cntAlphabet[idxAlphabet]++;
   }
+  return cntAlphabet.join('#');
 };
+
+const groupAnagrams = function (strs) {
+  const map = new Map();
+  strs.forEach(str => {
+    const key = generateKey(str);
+    if (map.has(key)) {
+      const value = map.get(key);
+      value.push(str);
+      map.set(key, value);
+    } else {
+      map.set(key, [str]);
+    }
+  });
+  return [...map.values()];
+};
+
+console.log(groupAnagrams(strs));
