6f4f186f071ee0c8711a5d3f398361b24550b1e6
Chris Choi <chrischoi3131@gmail.com>
2022-05-05 11:52:27 -0700

    https://leetcode.com/problems/top-k-frequent-elements/solution/ Data Structure-Heap, Hashmap:js

--- 01 ing/LeetCode/Main.js
+++ 01 ing/LeetCode/Main.js
@@ -1,33 +1,55 @@
-class ValidWordAbbr {
-  constructor(words) {
-    this.wordAbbrs = new Map();
-    words.forEach(word => {
-      const [key, value] = this.generateWordAbbr(word);
-      if (this.wordAbbrs.has(key) && this.wordAbbrs.get(key) !== value) this.wordAbbrs.set(key, '');
-      else this.wordAbbrs.set(key, value);
-    });
+const nums = [1],
+  k = 1;
+
+const topKFrequent = function (nums, k) {
+  const heap = new Array(nums.length).fill({ cnt: 0 });
+  let idxHeap = 0;
+  const h_push = cntNum => {
+    heap[++idxHeap] = cntNum;
+    for (let i = idxHeap; i > 1; i = Math.floor(i / 2)) {
+      const cnt = heap[i].cnt;
+      const cntParent = heap[Math.floor(i / 2)].cnt;
+      if (cnt > cntParent) {
+        const tmp = heap[i];
+        heap[i] = heap[Math.floor(i / 2)];
+        heap[Math.floor(i / 2)] = tmp;
+      } else break;
+    }
+  };
+  const h_pop = () => {
+    const cntNum = heap[1];
+    heap[1] = heap[idxHeap];
+    heap[idxHeap--] = { cnt: 0 };
+    for (let i = 1; i < idxHeap; ) {
+      if (heap[i].cnt > heap[i * 2].cnt && heap[i].cnt > heap[i * 2 + 1].cnt) break;
+      else if (heap[i * 2].cnt > heap[i * 2 + 1].cnt) {
+        const tmp = heap[i];
+        heap[i] = heap[i * 2];
+        heap[i * 2] = tmp;
+        i = i * 2;
+      } else {
+        const tmp = heap[i];
+        heap[i] = heap[i * 2 + 1];
+        heap[i * 2 + 1] = tmp;
+        i = i * 2 + 1;
+      }
+    }
+    return cntNum;
+  };
+
+  const cntNums = new Map();
+  nums.forEach(num => {
+    if (cntNums.has(num)) cntNums.set(num, cntNums.get(num) + 1);
+    else cntNums.set(num, 1);
+  });
+  cntNums.forEach((cnt, num) => {
+    h_push({ num, cnt });
+  });
+  const ans = [];
+  for (let i = 0; i < k; i++) {
+    ans.push(h_pop().num);
   }
-}
-ValidWordAbbr.prototype.generateWordAbbr = function (word) {
-  if (word.length <= 2) {
-    return [word, word];
-  } else {
-    const key = word.charAt(0) + (word.length - 2) + word.charAt(word.length - 1);
-    return [key, word];
-  }
-};
-ValidWordAbbr.prototype.isUnique = function (word) {
-  const [key, value] = this.generateWordAbbr(word);
-  if (this.wordAbbrs.has(key)) {
-    const wordSaved = this.wordAbbrs.get(key);
-    if (word === wordSaved) return true;
-    else return false;
-  } else return true;
+  return ans;
 };
 
-const validWordAbbr = new ValidWordAbbr(['a', 'a']);
-console.log(validWordAbbr.isUnique('a'));
-console.log(validWordAbbr.isUnique('door'));
-console.log(validWordAbbr.isUnique('cart'));
-// console.log(validWordAbbr.isUnique('make'));
-console.log(validWordAbbr.isUnique('cake'));
+console.log(topKFrequent(nums, k));
