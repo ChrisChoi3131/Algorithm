62ffb26a633e0ec191d60f894f03742dc9b2e013
Chris Choi <chrischoi3131@gmail.com>
2022-05-04 22:17:43 -0700

    https://leetcode.com/problems/jewels-and-stones/ Data Structure-hashset:js

--- 01 ing/LeetCode/Main.js
+++ 01 ing/LeetCode/Main.js
@@ -1,36 +1,18 @@
-const board = [
-  ['8', '3', '.', '.', '7', '.', '.', '.', '.'],
-  ['6', '.', '.', '1', '9', '5', '.', '.', '.'],
-  ['.', '9', '8', '.', '.', '.', '.', '6', '.'],
-  ['8', '.', '.', '.', '6', '.', '.', '.', '3'],
-  ['4', '.', '.', '8', '.', '3', '.', '.', '1'],
-  ['7', '.', '.', '.', '2', '.', '.', '.', '6'],
-  ['.', '6', '.', '.', '.', '.', '2', '8', '.'],
-  ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
-  ['.', '.', '.', '.', '8', '.', '.', '7', '9'],
-];
-
 /**
- * @param {character[][]} board
- * @return {boolean}
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
  */
-const isValidSudoku = function (board) {
-  const rows = new Array(9).fill(0).map(() => new Set());
-  const cols = new Array(9).fill(0).map(() => new Set());
-  const subBoxes = new Array(9).fill(0).map(() => new Set());
-  for (let i = 0; i < 9; i++) {
-    for (let j = 0; j < 9; j++) {
-      if (board[i][j] === '.') continue;
-      const num = Number(board[i][j]);
-      const IdxSubBox = 3 * Math.floor(i / 3) + Math.floor(j / 3);
-      if (rows[i].has(num) || cols[j].has(num) || subBoxes[IdxSubBox].has(num)) return false;
-      else {
-        rows[i].add(num);
-        cols[j].add(num);
-        subBoxes[IdxSubBox].add(num);
-      }
-    }
+const jewels = 'z',
+  stones = 'ZZ';
+
+const numJewelsInStones = function (jewels, stones) {
+  let numJewelsInStones = 0;
+  const setJewels = new Set(jewels.split(''));
+  for (const stone of stones.split('')) {
+    if (setJewels.has(stone)) numJewelsInStones++;
   }
-  return true;
+
+  return numJewelsInStones;
 };
-console.log(isValidSudoku(board));
+console.log(numJewelsInStones(jewels, stones));
