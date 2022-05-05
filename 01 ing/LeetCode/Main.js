e6cb1c2960a4417bf58fc065416af8adc5115ec9
Chris Choi <chrischoi3131@gmail.com>
2022-05-04 18:06:04 -0700

    https://leetcode.com/problems/group-shifted-strings/ Data Structure-Hashtable:JS

--- 01 ing/LeetCode/Main.js
+++ 01 ing/LeetCode/Main.js
@@ -1,29 +1,29 @@
-const strs = [''];
-// const strs = ['eat', 'tea', 'tan', 'ate', 'nat', 'bat'];
+/**
+ * @param {string[]} strings
+ * @return {string[][]}
+ */
+const strings = ['bac', 'bcd', 'acef', 'xyz', 'az', 'ba', 'a', 'z'];
 
 const generateKey = function (str) {
-  const strSplited = str.split('');
-  const cntAlphabet = new Array(26).fill(0);
-  for (let i = 0; i < strSplited.length; i++) {
-    const idxAlphabet = strSplited[i].charCodeAt() - 97;
-    cntAlphabet[idxAlphabet]++;
-  }
-  return cntAlphabet.join('#');
-};
-
-const groupAnagrams = function (strs) {
-  const map = new Map();
-  strs.forEach(str => {
-    const key = generateKey(str);
-    if (map.has(key)) {
-      const value = map.get(key);
-      value.push(str);
-      map.set(key, value);
-    } else {
-      map.set(key, [str]);
+  const charCodes = str.split('').map(char => char.charCodeAt());
+  while (charCodes[0] !== 0) {
+    for (let i = 0; i < charCodes.length; i++) {
+      charCodes[i]--;
+      if (charCodes[i] === -1) charCodes[i] = 25;
     }
-  });
-  return [...map.values()];
+  }
+  return charCodes.join('#');
 };
-
-console.log(groupAnagrams(strs));
+const groupStrings = function (strings) {
+  const groupShiftedStrs = new Map();
+  strings.forEach(str => {
+    const key = generateKey(str);
+    if (groupShiftedStrs.has(key)) {
+      const value = groupShiftedStrs.get(key);
+      value.push(str);
+      groupShiftedStrs.set(key, value);
+    } else groupShiftedStrs.set(key, [str]);
+  });
+  return [...groupShiftedStrs.values()];
+};
+console.log(groupStrings(strings));
