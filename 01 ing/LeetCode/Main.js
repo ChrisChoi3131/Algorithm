cd880a208c97c77f8800b857a441d79791628c9f
Chris Choi <chrischoi3131@gmail.com>
2022-05-05 14:24:12 -0700

    https://leetcode.com/problems/max-consecutive-ones/ Data Structure-Array:js

--- 01 ing/LeetCode/Main.js
+++ 01 ing/LeetCode/Main.js
@@ -1,32 +1,17 @@
-class RandomizedSet {
-  constructor() {
-    this.set = new Set();
-  }
-}
-RandomizedSet.prototype.insert = function (val) {
-  if (this.set.has(val)) return false;
-  else {
-    this.set.add(val);
-    return true;
-  }
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findMaxConsecutiveOnes = function (nums) {
+  let maxNumConsecutive = 0,
+    numConsecutive = 0;
+
+  nums.forEach(num => {
+    if (num === 1) numConsecutive++;
+    else numConsecutive = 0;
+    if (numConsecutive > maxNumConsecutive) maxNumConsecutive = numConsecutive;
+  });
+  return maxNumConsecutive;
 };
 
-RandomizedSet.prototype.remove = function (val) {
-  if (this.set.has(val)) {
-    this.set.delete(val);
-    return true;
-  } else return false;
-};
-
-RandomizedSet.prototype.getRandom = function () {
-  const randomIdx = Math.floor(Math.random() * [...this.set.keys()].length);
-  return [...this.set.keys()][randomIdx];
-};
-const randomizedSet = new RandomizedSet();
-console.log(randomizedSet.insert(1));
-console.log(randomizedSet.remove(2));
-console.log(randomizedSet.insert(2));
-console.log(randomizedSet.getRandom());
-console.log(randomizedSet.remove(1));
-console.log(randomizedSet.insert(2));
-console.log(randomizedSet.getRandom());
+console.log(findMaxConsecutiveOnes([1, 0, 1, 1, 1, 0, 1]));
