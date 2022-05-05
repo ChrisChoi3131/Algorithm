c1ee01e148033e59546d5a994ea6b87a67ff3b36
Chris Choi <chrischoi3131@gmail.com>
2022-05-05 10:35:36 -0700

    https://leetcode.com/problems/unique-word-abbreviation/ Data Structure-hashmap:js

--- 01 ing/LeetCode/Main.js
+++ 01 ing/LeetCode/Main.js
@@ -1,18 +1,33 @@
-/**
- * @param {string} jewels
- * @param {string} stones
- * @return {number}
- */
-const jewels = 'z',
-  stones = 'ZZ';
-
-const numJewelsInStones = function (jewels, stones) {
-  let numJewelsInStones = 0;
-  const setJewels = new Set(jewels.split(''));
-  for (const stone of stones.split('')) {
-    if (setJewels.has(stone)) numJewelsInStones++;
+class ValidWordAbbr {
+  constructor(words) {
+    this.wordAbbrs = new Map();
+    words.forEach(word => {
+      const [key, value] = this.generateWordAbbr(word);
+      if (this.wordAbbrs.has(key) && this.wordAbbrs.get(key) !== value) this.wordAbbrs.set(key, '');
+      else this.wordAbbrs.set(key, value);
+    });
   }
-
-  return numJewelsInStones;
+}
+ValidWordAbbr.prototype.generateWordAbbr = function (word) {
+  if (word.length <= 2) {
+    return [word, word];
+  } else {
+    const key = word.charAt(0) + (word.length - 2) + word.charAt(word.length - 1);
+    return [key, word];
+  }
 };
-console.log(numJewelsInStones(jewels, stones));
+ValidWordAbbr.prototype.isUnique = function (word) {
+  const [key, value] = this.generateWordAbbr(word);
+  if (this.wordAbbrs.has(key)) {
+    const wordSaved = this.wordAbbrs.get(key);
+    if (word === wordSaved) return true;
+    else return false;
+  } else return true;
+};
+
+const validWordAbbr = new ValidWordAbbr(['a', 'a']);
+console.log(validWordAbbr.isUnique('a'));
+console.log(validWordAbbr.isUnique('door'));
+console.log(validWordAbbr.isUnique('cart'));
+// console.log(validWordAbbr.isUnique('make'));
+console.log(validWordAbbr.isUnique('cake'));
