ae106ba4febec3f3dc568b8ddff97962ac4b70d3
Chris Choi <chrischoi3131@gmail.com>
2022-05-05 19:56:06 -0700

    https://leetcode.com/problems/sort-array-by-parity/ Array:js

--- 01 ing/LeetCode/Main.js
+++ 01 ing/LeetCode/Main.js
@@ -1,21 +1,32 @@
-const nums = [0, 1];
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
 
-const moveZeroes = function (nums) {
+// const nums = [0, 2, 1, 4];
+const nums = [3, 1, 2, 4];
+
+const sortArrayByParity = function (nums) {
   let pointerFirst = 0;
   let pointerSecond = 0;
   while (pointerFirst !== nums.length && pointerSecond !== nums.length) {
-    if (nums[pointerFirst] === 0 && nums[pointerSecond] === 0) {
+    if (nums[pointerFirst] % 2 === 1 && nums[pointerSecond] % 2 === 1) {
       pointerSecond++;
-    } else if (nums[pointerFirst] === 0 && nums[pointerSecond] !== 0) {
-      nums[pointerFirst] = nums[pointerSecond];
-      nums[pointerSecond] = 0;
-    } else if (nums[pointerFirst] !== 0 && nums[pointerSecond] === 0) {
+    } else if (nums[pointerFirst] % 2 === 0 && nums[pointerSecond] % 2 === 1) {
       pointerFirst++;
-    } else {
+      pointerSecond++;
+    } else if (nums[pointerFirst] % 2 === 1 && nums[pointerSecond] % 2 === 0) {
+      if (pointerFirst < pointerSecond) {
+        const tmp = nums[pointerFirst];
+        nums[pointerFirst] = nums[pointerSecond];
+        nums[pointerSecond] = tmp;
+      } else pointerSecond++;
+    }
+    if (nums[pointerFirst] % 2 === 0 && nums[pointerSecond] % 2 === 0) {
       pointerFirst++;
-      pointerSecond < nums.length - 1 ? pointerSecond++ : null;
     }
   }
   return nums;
 };
-moveZeroes(nums);
+
+console.log(sortArrayByParity(nums));
