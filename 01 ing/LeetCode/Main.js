644e02108852c64ec3e3c9ff0d173e17fe245760
Chris Choi <chrischoi3131@gmail.com>
2022-05-05 17:19:16 -0700

    https://leetcode.com/problems/remove-duplicates-from-sorted-array/ Array, Two Pointer:js

--- 01 ing/LeetCode/Main.js
+++ 01 ing/LeetCode/Main.js
@@ -1,10 +1,16 @@
-const removeElement = function (nums, val) {
-  let i = 0;
-  for (let j = 0; j < nums.length; j++) {
-    if (nums[j] !== val) {
+const nums = [1, 1, 2];
+
+const removeDuplicates = function (nums) {
+  let pValue = nums[0];
+  let i = 1;
+  for (let j = 1; j < nums.length; j++) {
+    if (pValue !== nums[j]) {
       nums[i] = nums[j];
+      pValue = nums[j];
       i++;
     }
   }
   return i;
 };
+
+removeDuplicates(nums);
