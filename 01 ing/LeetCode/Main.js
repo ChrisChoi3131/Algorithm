f40e3162615b4000ac24bd65b2e3f3323ef23576
Chris Choi <chrischoi3131@gmail.com>
2022-05-05 18:09:15 -0700

    https://leetcode.com/problems/check-if-n-and-its-double-exist/Hashtable:js

--- 01 ing/LeetCode/Main.js
+++ 01 ing/LeetCode/Main.js
@@ -1,16 +1,11 @@
-const nums = [1, 1, 2];
+const arr = [3, 1, 7, 11];
+// const arr = [10, 2, 5, 3];
 
-const removeDuplicates = function (nums) {
-  let pValue = nums[0];
-  let i = 1;
-  for (let j = 1; j < nums.length; j++) {
-    if (pValue !== nums[j]) {
-      nums[i] = nums[j];
-      pValue = nums[j];
-      i++;
-    }
+const checkIfExist = function (arr) {
+  const map = new Map(arr.map((num, idx) => [num, idx]));
+  for (let i = 0; i < arr.length; i++) {
+    if (map.has(arr[i] * 2) && map.get(arr[i] * 2) !== i) return true;
   }
-  return i;
+  return false;
 };
-
-removeDuplicates(nums);
+console.log(checkIfExist(arr));
