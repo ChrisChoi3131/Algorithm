2cae62dc92278a9d79c833faa314eac8f1aae680
Chris Choi <chrischoi3131@gmail.com>
2022-05-06 14:05:14 -0700

    https://leetcode.com/problems/max-consecutive-ones-ii/ DP:js

--- 01 ing/LeetCode/Main.js
+++ 01 ing/LeetCode/Main.js
@@ -1,12 +1,18 @@
-const nums = [2, 2, 3, 1];
+const nums = [1, 0, 1, 1, 0, 1];
 
-const thirdMax = function (nums) {
-  const set = new Set();
-  for (let i = 0; i < nums.length; i++) {
-    set.add(nums[i]);
-    if (set.size > 3) set.delete(Math.min(...set.values()));
+const findMaxConsecutiveOnes = function (nums) {
+  const d = new Array(nums.length).fill(0).map(() => new Array(2).fill(0));
+  d[0][0] = nums[0];
+  d[0][1] = 1;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] === 1) {
+      d[i][0] = d[i - 1][0] + 1;
+      d[i][1] = d[i - 1][1] + 1;
+    } else {
+      d[i][0] = 0;
+      d[i][1] = d[i - 1][0] + 1;
+    }
   }
-  if (set.size >= 3) return Math.min(...set.values());
-  else return Math.max(...set.values());
+  return Math.max(...d.flat());
 };
-thirdMax(nums);
+findMaxConsecutiveOnes(nums);
