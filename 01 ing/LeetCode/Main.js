f9898f2cfc73b2a55df7fd219d61f2e978a3eef2
Chris Choi <chrischoi3131@gmail.com>
2022-05-06 14:54:01 -0700

    https://leetcode.com/problems/largest-number-at-least-twice-of-others/ Array:js

--- 01 ing/LeetCode/Main.js
+++ 01 ing/LeetCode/Main.js
@@ -1,14 +1,12 @@
-const digits = [1, 2, 3];
-const plusOne = function (digits) {
-  let doAdd = true;
-  for (let i = digits.length - 1; i >= 0; i--) {
-    if (doAdd) digits[i]++;
-    else continue;
-    if (digits[i] >= 10) {
-      digits[i] -= 10;
-    } else doAdd = false;
+const nums = [3, 6, 1, 0];
+const dominantIndex = function (nums) {
+  const largestNum = Math.max(...nums);
+  let IdxLargestNum = -1;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== largestNum) {
+      if (largestNum / 2 < nums[i]) return -1;
+    } else IdxLargestNum = i;
   }
-  if (doAdd) return [1, ...digits];
-  else return [...digits];
+  return IdxLargestNum;
 };
-plusOne(digits);
+console.log(dominantIndex(nums));
