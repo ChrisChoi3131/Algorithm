abff5309183702191dd1fabc756812def0ebb3a0
Chris Choi <chrischoi3131@gmail.com>
2022-05-06 10:04:41 -0700

    https://leetcode.com/problems/third-maximum-number/ Data Structure-Set:js

--- 01 ing/LeetCode/Main.js
+++ 01 ing/LeetCode/Main.js
@@ -1,32 +1,12 @@
-/**
- * @param {number[]} nums
- * @return {number[]}
- */
+const nums = [2, 2, 3, 1];
 
-// const nums = [0, 2, 1, 4];
-const nums = [3, 1, 2, 4];
-
-const sortArrayByParity = function (nums) {
-  let pointerFirst = 0;
-  let pointerSecond = 0;
-  while (pointerFirst !== nums.length && pointerSecond !== nums.length) {
-    if (nums[pointerFirst] % 2 === 1 && nums[pointerSecond] % 2 === 1) {
-      pointerSecond++;
-    } else if (nums[pointerFirst] % 2 === 0 && nums[pointerSecond] % 2 === 1) {
-      pointerFirst++;
-      pointerSecond++;
-    } else if (nums[pointerFirst] % 2 === 1 && nums[pointerSecond] % 2 === 0) {
-      if (pointerFirst < pointerSecond) {
-        const tmp = nums[pointerFirst];
-        nums[pointerFirst] = nums[pointerSecond];
-        nums[pointerSecond] = tmp;
-      } else pointerSecond++;
-    }
-    if (nums[pointerFirst] % 2 === 0 && nums[pointerSecond] % 2 === 0) {
-      pointerFirst++;
-    }
+const thirdMax = function (nums) {
+  const set = new Set();
+  for (let i = 0; i < nums.length; i++) {
+    set.add(nums[i]);
+    if (set.size > 3) set.delete(Math.min(...set.values()));
   }
-  return nums;
+  if (set.size >= 3) return Math.min(...set.values());
+  else return Math.max(...set.values());
 };
-
-console.log(sortArrayByParity(nums));
+thirdMax(nums);
