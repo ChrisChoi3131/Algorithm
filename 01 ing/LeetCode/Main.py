926134db505c94b1ae0f916788cbddce7e1d294b
Chris Choi <chrischoi3131@gmail.com>
2022-05-28 15:25:32 -0700

    https://leetcode.com/problems/find-pivot-index/ python3

--- 01 ing/LeetCode/Main.py
+++ 01 ing/LeetCode/Main.py
@@ -1,29 +1,17 @@
-import sys
+class Solution:
+    def pivotIndex(self, nums: list[int]) -> int:
+        sumFromLeft, sumFromRight = [0] * len(nums), [0] * len(nums)
+        sumFromLeft[0], sumFromRight[len(nums)-1] = nums[0], nums[len(nums)-1]
+        for idx in range(1, len(nums)):
+            sumFromLeft[idx] = sumFromLeft[idx-1] + nums[idx]
+        for idx in range(len(nums)-2, -1, -1):
+            sumFromRight[idx] = sumFromRight[idx+1] + nums[idx]
 
-# input = sys.stdin.readline
-file = open("./01 ing/LeetCode/sample.txt", "r")
-input = file.readline
+        for idx in range(0, len(nums)):
+            if(sumFromLeft[idx] == sumFromRight[idx]):
+                return idx
+        return -1
 
-n = int(input())
-stack = []
-for _ in range(n):
-    s = input().split()
-    cmd = s[0]
-    if(cmd == "push"):
-        stack.append(s[1])
-    elif(cmd == "pop" and len(stack) == 0):
-        print(-1)
-    elif(cmd == "pop" and len(stack) != 0):
-        print(stack.pop())
-    elif(cmd == "size"):
-        print(len(stack))
-    elif(cmd == "empty" and len(stack) == 0):
-        print(1)
-    elif(cmd == "empty" and len(stack) != 0):
-        print(0)
-    elif(cmd == "top" and len(stack) == 0):
-        print(-1)
-    elif(cmd == "top" and len(stack) != 0):
-        print(stack[len(stack)-1])
 
-file.close()
+sol = Solution()
+print(sol.pivotIndex([-1, 1, 2]))
